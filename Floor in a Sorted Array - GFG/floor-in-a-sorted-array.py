1b9434da89ace3db3dd45d1765d86b212588461c
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-06-06 23:18:39 +0530

    Added solution - LeetHub

--- Floor in a Sorted Array - GFG/floor-in-a-sorted-array.py
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.py
@@ -1,26 +1,27 @@
 class Solution:
-    #User function Template for python3
-    
-    #Complete this function
-    def findFloor(self,A,N,X):
+    # User function Template for python3
+
+    # Complete this function
+    def findFloor(self, A, N, X):
         low = 0
-        high = N-1
+        high = N - 1
         ans = N
+        # Check if X is smaller than the first element in the array
         if X < A[0]:
             return -1
+        # Check if X is greater than the last element in the array
         if X > A[high]:
             return high
         while low <= high:
-            mid = (low+high) //2
+            mid = (low + high) // 2
             if A[mid] == X:
                 return mid
             elif A[mid] >= X:
                 ans = mid
-                high = mid-1
+                high = mid - 1  # Discard the upper half of the array
             else:
-                low = mid+1
+                low = mid + 1  # Discard the lower half of the array
         return ans
-
 
 #{ 
  # Driver Code Starts
