92c7210ae9306833b92959502e0030cf06e08a35
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-06-15 23:16:38 +0530

    Added solution - LeetHub

---
+++ Second Largest - GFG/second-largest.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+class Solution:
+    def print2largest(self, arr, n):
+        largest = arr[0]
+        slargest = -1
+        for i in range(1, len(arr)):
+            if arr[i] > largest:
+                slargest = largest
+                largest = arr[i]
+            elif largest > arr[i] > slargest:
+                slargest = arr[i]
+        return slargest
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.print2largest(arr, n)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
