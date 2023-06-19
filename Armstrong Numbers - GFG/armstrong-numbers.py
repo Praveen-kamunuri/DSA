493fbdd40259bf00901fb14678a1d9c62b88b3ed
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-06-19 23:14:49 +0530

    Added solution - LeetHub

--- Armstrong Numbers - GFG/armstrong-numbers.py
+++ Armstrong Numbers - GFG/armstrong-numbers.py
@@ -2,16 +2,20 @@
 
 #User function Template for python3
 class Solution:
-    def armstrongNumber (ob, n):
-        # code here 
-        res = 0
-        num_char = str(n)
+    def armstrongNumber(ob, n):
+        res = 0  # Initialize the variable to store the sum of cube values
+        num_char = str(n)  # Convert the number to a string to access individual digits
+
+        # Iterate through each digit in the number
         for i in range(len(num_char)):
-            res = res + int(num_char[i]) ** 3
+            res += int(num_char[i]) ** 3  # Calculate the cube of the digit and add it to the sum
+
+        # Check if the sum of cube values is equal to the original number
         if res == n:
-            return "Yes"
+            return "Yes"  # Return "Yes" if it is an Armstrong number
         else:
-            return "No"
+            return "No"  # Return "No" if it is not an Armstrong number
+
             
 
 
