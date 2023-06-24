6e085ea1325322447f42826a6d7e13526dc84ed9
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-06-24 23:00:01 +0530

    Time: 31 ms (84.02%), Space: 15.6 MB (22.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,4 +1,7 @@
 class Solution(object):
     def isPalindrome(self, s):
-        modifi = ''.join(c.lower() for c in s if c.isalnum())
-        return modifi == modifi[::-1]
+        # Convert to lowercase and remove non-alphanumeric characters
+        modified = ''.join(c.lower() for c in s if c.isalnum())
+        
+        # Compare with the reversed string
+        return modified == modified[::-1]
