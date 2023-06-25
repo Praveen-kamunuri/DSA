28993680f7abfc0b4244e3461dab153d4fc9add8
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-06-25 11:26:51 +0530

    Time: 721 ms (25.77%), Space: 13.2 MB (72.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def fib(self, n):
+        # Base cases: if n is 0 or 1, return n
         if n <= 1:
             return n
         else:
+            # Recursive calls to calculate Fibonacci numbers
             last = self.fib(n-1)
             slast = self.fib(n-2)
+        # Return the sum of the last two Fibonacci numbers
         return last + slast
-        
-        
