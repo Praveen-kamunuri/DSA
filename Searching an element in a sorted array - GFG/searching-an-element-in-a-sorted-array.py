9dd099200997ac1a6dd07338fd519dde64e1c97c
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-07-08 00:40:56 +0530

    Added solution - LeetHub

--- Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.py
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.py
@@ -2,15 +2,16 @@
 
 class Solution:
     ##Complete this function
-    def searchInSorted(self,arr, N, K):
-        #Your code here
-        i = 0
-        while (i < N):
-            if arr[i] != K:
-                i += 1
-            elif arr[i] == K:
-                return 1
-        return -1
+    def searchInSorted(self, arr, N, K):
+        # Your code here
+        i = 0  # Initialize index variable to 0
+        while (i < N):  # Start a while loop with the condition (i < N)
+            if arr[i] != K:  # Check if element at index i is not equal to K
+                i += 1  # Increment i by 1 if the element is not equal to K
+            elif arr[i] == K:  # Check if element at index i is equal to K
+                return 1  # Return 1 if the element is found
+        return -1  # Return -1 if the element is not found
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
