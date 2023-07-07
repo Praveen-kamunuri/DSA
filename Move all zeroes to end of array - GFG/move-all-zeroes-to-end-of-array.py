62be81e10e950be6e4777367ad249a938c93c138
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-07-08 00:26:22 +0530

    Added solution - LeetHub

--- Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.py
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.py
@@ -1,18 +1,23 @@
 #User function Template for python3
 
 class Solution:
-	def pushZerosToEnd(self,arr, n):
-    	# code here
-    	count = 0
-    	for i in range(n):
-    	    if arr[i] != 0:
-    	        arr[count] = arr[i]
-    	        count += 1
-    	while count < n:
-    	    arr[count] = 0
-    	    count += 1
-    	return arr
-    	   
+    def pushZerosToEnd(self, arr, n):
+        # code here
+        count = 0  # variable to keep track of non-zero elements
+
+        # Traverse the array and move non-zero elements to the front
+        for i in range(n):
+            if arr[i] != 0:
+                arr[count] = arr[i]
+                count += 1
+
+        # Fill the remaining positions with zeros
+        while count < n:
+            arr[count] = 0
+            count += 1
+
+        return arr
+
     	       
     	        
     	    
