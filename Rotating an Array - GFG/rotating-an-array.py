8ab479390eac7e8f3cd2b08cc77d10f96c990ed0
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-07-08 00:04:33 +0530

    Added solution - LeetHub

--- Rotating an Array - GFG/rotating-an-array.py
+++ Rotating an Array - GFG/rotating-an-array.py
@@ -3,17 +3,32 @@
 class Solution:
     def leftRotate(self, arr, n, d):
         # code here
+        
+        # Calculate the effective number of rotations
         d = d % n
+        
+        # Create an empty list to store the elements to be shifted
         li = []
+        
+        # Store the first d+1 elements in the list
         for i in range(d+1):
             li.append(arr[i])
-        for i in range(d,n):
+        
+        # Shift the remaining elements by d positions to the left
+        for i in range(d, n):
             arr[i-d] = arr[i]
+        
+        # Calculate the starting index for placing the shifted elements
         k = n-d
-        for i in range(n-d,n):
+        
+        # Place the shifted elements at the end of the array
+        for i in range(n-d, n):
             arr[i] = li[k - (n - d)]
             k += 1
+        
+        # Return the modified array
         return arr
+
        
 
 #{ 
