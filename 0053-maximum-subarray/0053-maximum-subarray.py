d91ee6c4dd0e037dfd4a692f45b837a78815749e
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-07-16 11:13:10 +0530

    Time: 587 ms (87.68%), Space: 26.5 MB (8.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,18 +1,11 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        # Initialize max_sum and current_sum with the first element of the array
-        max_sum = nums[0]
-        current_sum = nums[0]
-
-        # Iterate over the array starting from the second element
-        for i in range(1, len(nums)):
-            # Calculate the new current_sum by choosing the maximum between the current element and the sum of the current element and previous subarray
-            current_sum = max(nums[i], current_sum + nums[i])
-
-            # Update max_sum if the current_sum is greater
-            max_sum = max(max_sum, current_sum)
-
-        # Return the maximum sum found
-        return max_sum
-
-       
+        cur_sum = 0
+        maxi = nums[0]
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            if cur_sum > maxi:
+                maxi =  cur_sum
+            if cur_sum < 0:
+                cur_sum = 0
+        return maxi
