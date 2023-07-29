3efa21edc734940e90dcfda96095b1cc8e2fbe8e
Praveen-kamunuri <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-07-29 12:23:29 +0530

    Time: 100 ms (88.07%), Space: 14.3 MB (25.32%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,15 +2,21 @@ class Solution(object):
     def setZeroes(self, matrix):
         rows = len(matrix)
         cols = len(matrix[0])
-        zero_rows = set()
-        zero_cols = set()
+        
+        zeroRows = set()
+        zeroCols = set()
+        
+        # Identify the rows and columns containing zeros
         for i in range(rows):
             for j in range(cols):
                 if matrix[i][j] == 0:
-                    zero_rows.add(i)
-                    zero_cols.add(j)
+                    zeroRows.add(i)
+                    zeroCols.add(j)
+        
+        # Set the corresponding rows and columns to zeros
         for i in range(rows):
             for j in range(cols):
-                if i in zero_rows or j in zero_cols:
+                if i in zeroRows or j in zeroCols:
                     matrix[i][j] = 0
-                    
+        
+        return matrix
