62086f8baa2b56dfc90a56ae3ded0bd6c3309ee4
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-07 21:14:18 +0530

    Time: 13 ms (81.57%), Space: 13.1 MB (92.60%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,33 +1,44 @@
 class Solution(object):
     def spiralOrder(self, matrix):
-        n = len(matrix)
-        m = len(matrix[0])
-        top = 0
-        bottom = n - 1
-        left = 0
-        right = m - 1
+        # Define ans array to store the result.
         ans = []
+        n = len(matrix) # no. of rows
+        m = len(matrix[0]) # no. of columns
         
-        while top <= bottom and left <= right:
-            
-            for i in range(left,right+1):
-                ans.append(matrix[left][i])
+        # Initialize the pointers reqd for traversal.
+        top = 0
+        left = 0
+        bottom = n - 1
+        right = m - 1
+
+        # Loop until all elements are not traversed.
+        while (top <= bottom and left <= right):
+            # For moving left to right
+            for i in range(left, right + 1):
+                ans.append(matrix[top][i])
+
             top += 1
-            
-            for i in range(top,bottom+1):
+
+            # For moving top to bottom.
+            for i in range(top, bottom + 1):
                 ans.append(matrix[i][right])
+
             right -= 1
-            
-            if top <= bottom:
-                for i in range(right,left-1,-1):
+
+            # For moving right to left.
+            if (top <= bottom):
+                for i in range(right, left - 1, -1):
                     ans.append(matrix[bottom][i])
+
                 bottom -= 1
-            
-            if left <= right:
-                for i in range(bottom,top-1,-1):
+
+            # For moving bottom to top.
+            if (left <= right):
+                for i in range(bottom, top - 1, -1):
                     ans.append(matrix[i][left])
+
                 left += 1
+
         return ans
-                    
                 
         
