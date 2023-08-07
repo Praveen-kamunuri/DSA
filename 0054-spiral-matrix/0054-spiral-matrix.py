0b9de69232f659f2579dfb1fa8c30a6d1f8fd7a0
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-07 21:11:40 +0530

    Time: 17 ms (55.49%), Space: 13.3 MB (31.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -9,11 +9,11 @@ class Solution(object):
         ans = []
         
         while top <= bottom and left <= right:
-    
+            
             for i in range(left,right+1):
-                ans.append(matrix[top][i])
+                ans.append(matrix[left][i])
             top += 1
-
+            
             for i in range(top,bottom+1):
                 ans.append(matrix[i][right])
             right -= 1
@@ -27,7 +27,7 @@ class Solution(object):
                 for i in range(bottom,top-1,-1):
                     ans.append(matrix[i][left])
                 left += 1
-        
         return ans
-            
-            
+                    
+                
+        
