c262abcae05699cfb3e0aab66919f1000df16bbb
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-17 23:07:02 +0530

    Time: 617 ms (51.79%), Space: 13.2 MB (93.89%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,32 +1,40 @@
 class Solution(object):
     def fourSum(self, nums, target):
-        n = len(nums)
+        n = len(nums) # size of the array
         ans = []
+
+        # sort the given array:
         nums.sort()
+
+        # calculating the quadruplets:
         for i in range(n):
-            if i != 0 and nums[i] == nums[i-1]:
+            # avoid the duplicates while moving i:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i+1,n):
-                if j != i + 1 and nums[j] == nums[j-1]:
+            for j in range(i + 1, n):
+                # avoid the duplicates while moving j:
+                if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
+
+                # 2 pointers:
                 k = j + 1
                 l = n - 1
                 while k < l:
-                    total_sum = nums[i] + nums[j] + nums[k] + nums[l]
-                    if total_sum == target:
-                        temp = [nums[i],nums[j],nums[k],nums[l]]
+                    _sum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if _sum == target:
+                        temp = [nums[i], nums[j], nums[k], nums[l]]
                         ans.append(temp)
                         k += 1
                         l -= 1
-                        while k < l and nums[k] == nums[k-1]:
+
+                        # skip the duplicates:
+                        while k < l and nums[k] == nums[k - 1]:
                             k += 1
-                        while k < l and nums[l] == nums[l+1]:
+                        while k < l and nums[l] == nums[l + 1]:
                             l -= 1
-                    elif total_sum < target:
+                    elif _sum < target:
                         k += 1
                     else:
                         l -= 1
+
         return ans
-        
-                        
-            
