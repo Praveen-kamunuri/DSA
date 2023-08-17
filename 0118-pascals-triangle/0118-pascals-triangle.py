459190b9651cc7703a719aeeeef82d3f7fc3f391
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-17 19:27:11 +0530

    Time: 18 ms (62.70%), Space: 13.3 MB (26.19%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,26 +1,14 @@
 class Solution(object):
     def generate(self, numRows):
-        # Initialize the triangle with the first two rows
-        triangle = [[1], [1, 1]]
-        
-        # If numRows is 1, return the triangle with only the first row
+        triangle = [[1],[1,1]]
         if numRows == 1:
             return [[1]]
-        
-        # If numRows is 2, return the triangle with the first two rows
-        if numRows == 2:
-            return [[1], [1, 1]]
-        
-        # Generate the remaining rows
+        if numRows ==2:
+            return triangle
         for i in range(numRows-2):
-            row = [1]  # Start each row with 1
-            
-            # Calculate values for the current row based on the previous row
+            row = [1]
             for j in range(len(triangle[-1])-1):
-                # Each value is the sum of the current and next element from the previous row
                 row.append(triangle[-1][j]+triangle[-1][j+1])
-            
-            row.append(1)  # End each row with 1
-            triangle.append(row)  # Add the row to the triangle
-        
+            row.append(1)
+            triangle.append(row)
         return triangle
