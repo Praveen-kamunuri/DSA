3f31618a0e16dbac26ae7268c61d0da148c1a4f1
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-17 22:29:31 +0530

    Time: 852 ms (74.13%), Space: 16.9 MB (27.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,26 +2,35 @@ class Solution(object):
     def threeSum(self, nums):
         n = len(nums)
         ans = []
-        nums.sort()
+        nums.sort()  # Sort the input list
+        
         for i in range(n):
-            if i != 0  and nums[i] == nums[i-1]:
-                continue
-            j = i + 1
-            k = n - 1
+            if i != 0 and nums[i] == nums[i-1]:
+                continue  # Skip duplicates by continuing to the next iteration
+            
+            j = i + 1  # Start the second pointer at the element next to i
+            k = n - 1  # Start the third pointer at the last element
+            
             while j < k:
                 trip_sum = nums[i] + nums[j] + nums[k]
+                
                 if trip_sum < 0:
-                    j += 1
+                    j += 1  # Move the second pointer to the right
                 elif trip_sum > 0:
-                    k -= 1
+                    k -= 1  # Move the third pointer to the left
                 else:
-                    temp = [nums[i],nums[j],nums[k]]
+                    # Found a valid triple, add it to the answer
+                    temp = [nums[i], nums[j], nums[k]]
                     ans.append(temp)
                     j += 1
                     k -= 1
+                    
+                    # Skip duplicates by incrementing the second pointer
                     while j < k and nums[j] == nums[j-1]:
                         j += 1
+                        
+                    # Skip duplicates by decrementing the third pointer
                     while j < k and nums[k] == nums[k+1]:
                         k -= 1
+        
         return ans
-                        
