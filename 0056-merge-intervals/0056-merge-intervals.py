df30a0c22ea6065d7fb76583a71c3a9e817197ef
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-18 21:39:20 +0530

    Time: 108 ms (70.34%), Space: 18 MB (94.25%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def merge(self, intervals):
+        n =  len(intervals)
+        ans = []
+        intervals.sort()
+        for i in range(n):
+            if not ans or intervals[i][0] > ans[-1][1]:
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1] , intervals[i][1])
+        return ans
+        
+        
