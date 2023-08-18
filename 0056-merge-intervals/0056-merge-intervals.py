a64f359ff534f82414fa069455ae554e9b4225e4
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-18 21:43:15 +0530

    Time: 97 ms (92.12%), Space: 18.2 MB (41.19%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,10 +2,16 @@ class Solution(object):
     def merge(self, intervals):
         n = len(intervals)
         ans = []
+        
+        # Sort the intervals based on their start values
         intervals.sort()
+        
         for i in range(n):
+            # If the ans list is empty or there's a gap between intervals, add the current interval
             if not ans or intervals[i][0] > ans[-1][1]:
                 ans.append(intervals[i])
             else:
-                ans[-1][1] = max(ans[-1][1] , intervals[i][1])
+                # Merge overlapping intervals by updating the end value of the last interval
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
+                
         return ans
