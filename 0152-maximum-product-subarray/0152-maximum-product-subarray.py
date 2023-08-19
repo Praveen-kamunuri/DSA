35085d936757d15f45707fdb12860aa50d72cfec
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-19 22:02:15 +0530

    Time: 53 ms (78.27%), Space: 13.9 MB (37.24%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,16 +1,22 @@
 class Solution(object):
     def maxProduct(self, nums):
         n = len(nums)
-        prefix = 1 
-        suffix = 1
-        ans = float('-inf')
+        prefix = 1  # Initialize prefix product
+        suffix = 1  # Initialize suffix product
+        ans = float('-inf')  # Initialize answer to negative infinity
+        
+        # Iterate through the array
         for i in range(n):
+            # If prefix becomes 0, reset it to 1
             if prefix == 0:
                 prefix = 1
+            # If suffix becomes 0, reset it to 1
             if suffix == 0:
                 suffix = 1
+            # Update prefix and suffix products
             prefix *= nums[i]
-            suffix *= nums[n-i-1]
-            ans = max(ans , max(prefix , suffix) )
-        return ans
-    
+            suffix *= nums[n - i - 1]
+            # Update the answer with the maximum of current answer, prefix, and suffix
+            ans = max(ans, max(prefix, suffix))
+        
+        return ans  # Return the maximum product found
