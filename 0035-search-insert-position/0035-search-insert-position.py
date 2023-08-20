0f551fa1181ac6bbdcd3873edeadd413c94e4b9c
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-20 11:19:36 +0530

    Time: 23 ms (96.72%), Space: 13.9 MB (95.76%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,12 +3,14 @@ class Solution(object):
         n = len(nums)
         low = 0
         high = n - 1
-        low_bound = n 
+        ans = n
         while low <= high:
             mid = (low + high) //2
-            if nums[mid] < target:
-                low = mid+1
-            elif nums[mid] >= target:
-                low_bound = mid
+            if nums[mid] >= target:
+                ans = mid
                 high = mid - 1
-        return low_bound
+            elif nums[mid] < target:
+                low = mid + 1
+        return ans
+                
+                
