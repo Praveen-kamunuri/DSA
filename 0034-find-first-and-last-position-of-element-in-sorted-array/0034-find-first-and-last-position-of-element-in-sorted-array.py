1af58e63894852cd33ff4c36522133c4e14fe6d8
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-21 20:01:30 +0530

    Time: 52 ms (94.81%), Space: 14.3 MB (97.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,11 +3,11 @@ class Solution(object):
         n = len(nums)
         low = 0
         high = n - 1
-        first = -1
+        first = -1  # Initialize the index for the first occurrence of the target
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
-                first = mid
+                first = mid  # Update the first occurrence index and continue searching on the left side
                 high = mid - 1
             elif nums[mid] < target:
                 low = mid + 1
@@ -19,12 +19,12 @@ class Solution(object):
         n = len(nums)
         low = 0
         high = n - 1
-        end = -1
+        end = -1  # Initialize the index for the last occurrence of the target
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
-                end = mid
-                low  = mid + 1
+                end = mid  # Update the last occurrence index and continue searching on the right side
+                low = mid + 1
             elif nums[mid] < target:
                 low = mid + 1
             else:
@@ -33,7 +33,6 @@ class Solution(object):
                 
                 
     def searchRange(self, nums, target):
-        start = self.start_ind(nums,target)
-        end = self.end_ind(nums,target)
-        return [start,end]
-       
+        start = self.start_ind(nums, target)  # Get the index of the first occurrence of the target
+        end = self.end_ind(nums, target)  # Get the index of the last occurrence of the target
+        return [start, end]  # Return the indices of the target's occurrences
