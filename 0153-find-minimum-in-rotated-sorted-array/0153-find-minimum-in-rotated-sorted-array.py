df14c645f558a3b5105b1120261e49e75782e4ea
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-23 19:40:10 +0530

    Time: 22 ms (74.65%), Space: 13.4 MB (76.88%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,17 +1,31 @@
 import sys
+
 class Solution(object):
     def findMin(self, nums):
+        # Get the length of the input list
         n = len(nums)
+        
+        # Initialize pointers for binary search
         low = 0
         high = n - 1
-        ans  = sys.maxsize
+        
+        # Initialize the answer to a large value
+        ans = sys.maxsize
+        
+        # Perform binary search
         while low <= high:
-            mid = (low + high) //2
+            # Calculate mid index
+            mid = (low + high) // 2
+            
+            # Compare values at low and mid indices
             if nums[low] <= nums[mid]:
-                ans = min(ans , nums[low])
+                # Update answer and adjust low pointer
+                ans = min(ans, nums[low])
                 low = mid + 1
             else:
-                ans = min(ans,nums[mid])
-                high = mid  - 1
+                # Update answer and adjust high pointer
+                ans = min(ans, nums[mid])
+                high = mid - 1
+        
+        # Return the minimum element found
         return ans
-                
