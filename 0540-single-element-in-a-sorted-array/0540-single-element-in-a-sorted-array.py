03a2ace7c0cb37a448a9824e0762b885316cf3bf
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-08-23 22:46:26 +0530

    Time: 132 ms (68.15%), Space: 20.4 MB (61.29%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,32 +1,21 @@
 class Solution(object):
     def singleNonDuplicate(self, nums):
-        n = len(nums)  # Size of the array
-
-        # Edge cases:
+        n = len(nums)
         if n == 1:
             return nums[0]
         if nums[0] != nums[1]:
             return nums[0]
-        if nums[n - 1] != nums[n - 2]:
-            return nums[n - 1]
-
+        if nums[n-1] != nums[n-2]:
+            return nums[n-1]
         low = 1
         high = n - 2
         while low <= high:
-            mid = (low + high) // 2
-
-            # If arr[mid] is the single element:
-            if nums[mid] != nums[mid + 1] and nums[mid] != nums[mid - 1]:
+            mid = (low + high) //2
+            if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]:
                 return nums[mid]
-
-            # We are in the left:
-            if (mid % 2 == 1 and nums[mid] == nums[mid - 1]) or (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
-                # Eliminate the left half:
+            if (mid % 2 == 1 and nums[mid] == nums[mid-1]) or (mid % 2 == 0 and nums[mid] == nums[mid+1]):
                 low = mid + 1
-            # We are in the right:
             else:
-                # Eliminate the right half:
-                high = mid - 1
-
-        # Dummy return statement:
+                high = mid  - 1
         return -1
+                
