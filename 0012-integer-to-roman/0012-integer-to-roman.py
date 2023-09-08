f8fb114ca023b36b1b1cbcee3d4a05d5680ca5e0
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-08 19:43:19 +0530

    Time: 36 ms (42.07%), Space: 13.2 MB (85.81%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -2,15 +2,12 @@ class Solution(object):
     def intToRoman(self, num):
         val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
         sym = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
-        
         i = 0
-        
         ans = ""
         
         while num > 0:
             while num >= val[i]:
-                ans +=  sym[i]
+                ans += sym[i]
                 num -= val[i]
             i += 1
         return ans
-                
