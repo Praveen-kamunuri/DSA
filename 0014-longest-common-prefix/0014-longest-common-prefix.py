88fadf7d4dc55448364e3bcc743bc0698604ad10
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-13 23:24:32 +0530

    Time: 20 ms (42.80%), Space: 13.5 MB (79.73%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,14 +1,23 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        # Check if the input list is empty or contains an empty string
         if not strs or strs[0] == "":
             return ""
+        
+        # Check if there's only one string in the list
         if len(strs) == 1:
             return strs[0]
         
-        min_str = min(strs , key = len)
+        # Find the shortest string in the array (lexicographically)
+        min_str = min(strs, key=len)
         
-        for i , char in enumerate(min_str):
+        # Iterate through the characters of the shortest string
+        for i, char in enumerate(min_str):
+            # Compare the current character with the corresponding character in each string in 'strs'
             for s in strs:
                 if s[i] != char:
+                    # If a mismatch is found, return the common prefix up to this point
                     return min_str[:i]
+        
+        # If no mismatches are found, return the entire shortest string as the common prefix
         return min_str
