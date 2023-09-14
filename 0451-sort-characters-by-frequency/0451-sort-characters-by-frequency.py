94583cf9fa6fd58126578310e2400bade6f2807a
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-14 23:33:14 +0530

    Time: 46 ms (82.99%), Space: 17.6 MB (89.38%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,12 +1,23 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
+        # Create an empty dictionary to store character frequencies
         hashmap = {}
+        
+        # Calculate character frequencies and store them in the hashmap
         for i in s:
             if i in hashmap:
                 hashmap[i] += 1
             else:
                 hashmap[i] = 1
-        sorted_keys = sorted(hashmap.keys() , key = lambda k: hashmap[k] , reverse = True)
-        repeated_keys = [key * hashmap[key] for  key in sorted_keys]
+        
+        # Sort the keys (characters) based on their frequencies in decreasing order
+        sorted_keys = sorted(hashmap.keys(), key=lambda k: hashmap[k], reverse=True)
+        
+        # Create a list of characters repeated based on their frequencies
+        repeated_keys = [key * hashmap[key] for key in sorted_keys]
+        
+        # Concatenate the repeated characters to form the final result
         res = ''.join(repeated_keys)
+        
+        # Return the result as a string with characters sorted by frequency
         return res
