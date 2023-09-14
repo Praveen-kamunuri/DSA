0170542cc0193c528aff220a8222b2e6614c8fbf
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-14 22:58:18 +0530

    Time: 12 ms (79.35%), Space: 13.4 MB (13.00%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,23 +1,11 @@
 class Solution(object):
     def rotateString(self, s, goal):
-        if (len(s) != len(goal)):
+        if len(s) != len(goal):
             return False
-        q1 = []
-        for i in range(len(s)):
-            q1.insert(0, s[i])
-
-        q2 = []
-        for i in range(len(goal)):
-            q2.insert(0, goal[i])
-
-        k = len(goal)
-        while (k > 0):
-            ch = q2[0]
-            q2.pop(0)
-            q2.append(ch)
-            if (q2 == q1):
-                return True
-
-            k -= 1
-
-        return False
+        if s == goal:
+            return True
+        
+        if goal in s + s:
+            return True
+        else:
+            False
