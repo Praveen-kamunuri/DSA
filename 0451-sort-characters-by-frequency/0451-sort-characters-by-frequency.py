7e073a0871e6657e53fa878e9cc45e14169f7edb
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-14 23:21:05 +0530

    Time: 50 ms (69.91%), Space: 17.5 MB (89.38%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,12 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        hashmap = {}
+        for i in s:
+            if i in hashmap:
+                hashmap[i] += 1
+            else:
+                hashmap[i] = 1
+        sorted_keys = sorted(hashmap.keys() , key = lambda k: hashmap[k] , reverse = True)
+        rep_keys = [key * hashmap[key] for key in sorted_keys]
+        res = ''.join(rep_keys)
+        return res
