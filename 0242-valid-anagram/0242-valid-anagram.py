798d66ddcaf975352a2fc6c745b17050879b7cd4
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-15 22:25:42 +0530

    Time: 64 ms (24.02%), Space: 17.4 MB (28.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,4 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        # Sort the characters of both strings and compare if they are equal
-        if sorted(s) == sorted(t):
-            return True  # If sorted strings are equal, strings are anagrams
-        else:
-            return False  # If sorted strings are not equal, strings are not anagrams
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return  sorted(s) == sorted(t) 
+        
