c2b11d705e4a4b324ba5f63ff48932237093eac6
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-17 20:15:13 +0530

    Time: 34 ms (84.80%), Space: 16.3 MB (63.05%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,20 +1,20 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
+        # If the input string is empty, the maximum depth is 0.
+        if s == "":
+            return 0
         
-        
-        
-        curr_max = 0
-        maxi = 0
+        curr_max = 0  # Initialize a variable to keep track of the current depth.
+        maxi = 0      # Initialize a variable to store the maximum depth found.
+
+        # Iterate through each character in the input string.
         for i in s:
             if i == "(":
-                curr_max += 1
+                curr_max += 1  # Increment the current depth for an open parenthesis '('.
             elif i == ")":
-                maxi = max(curr_max , maxi)
-                curr_max -= 1
+                maxi = max(curr_max, maxi)  # Update the maximum depth if needed.
+                curr_max -= 1  # Decrement the current depth for a closing parenthesis ')'.
             else:
-                continue
-        return maxi
-            
-            
-                
-        
+                continue  # Ignore any other characters.
+
+        return maxi  # Return the maximum depth found.
