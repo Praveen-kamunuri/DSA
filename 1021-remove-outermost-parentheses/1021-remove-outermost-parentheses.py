4f72b1dc413c699563a6859f5b47fe392255915b
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-17 20:00:14 +0530

    Time: 45 ms (55.22%), Space: 16.4 MB (46.85%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,15 +1,16 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        res = []
-        bal = 0
+        res = []  # Initialize an empty list to store the result.
+        bal = 0   # Initialize a balance variable to keep track of parentheses balance.
+        
         for i in s:
             if i == "(":
                 if bal > 0:
-                    res.append(i)
-                bal += 1
+                    res.append(i)  # Append '(' to the result if it's not the outermost '('.
+                bal += 1  # Increment the balance for an open parenthesis '('.
             else:
-                bal -= 1
+                bal -= 1  # Decrement the balance for a closing parenthesis ')'.
                 if bal > 0:
-                    res.append(i)
-        return "".join(res)
+                    res.append(i)  # Append ')' to the result if it's not the outermost ')'.
         
+        return "".join(res)  # Convert the result list to a string and return it.
