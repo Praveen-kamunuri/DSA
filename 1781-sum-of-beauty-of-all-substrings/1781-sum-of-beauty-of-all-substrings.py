4aec36a07490c451155aa8ae9646e9e99daa6f04
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-21 22:05:15 +0530

    Time: 17756 ms (14.20%), Space: 16.1 MB (96.94%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -2,15 +2,15 @@ class Solution:
     def beautySum(self, s: str) -> int:
         def freq(string):
             hashmap = {}
-            for i in string:
-                if i in hashmap:
-                    hashmap[i] += 1
+            for char in string:
+                if char in hashmap:
+                    hashmap[char] += 1
                 else:
-                    hashmap[i] = 1
+                    hashmap[char] = 1
             maxi = max(hashmap.values())
             mini = min(hashmap.values())
             return maxi - mini
-            
+        
         
         
         
@@ -18,9 +18,10 @@ class Solution:
         
         total_sum = 0
         for i in range(len(s)):
-            for j in range(i+1,len(s)):
+            for j in  range(i+1,len(s)):
                 sub_str = s[i:j+1]
                 curr_freq = freq(sub_str)
                 total_sum += curr_freq
         return total_sum
                 
+        
