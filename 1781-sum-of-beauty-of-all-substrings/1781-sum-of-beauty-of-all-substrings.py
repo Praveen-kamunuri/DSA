6caa9323cd303c5215aa628e90a71f66c87533ad
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-21 22:06:25 +0530

    Time: 17667 ms (14.64%), Space: 16 MB (96.94%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,5 +1,6 @@
 class Solution:
     def beautySum(self, s: str) -> int:
+        # Helper function to calculate the beauty of a string
         def freq(string):
             hashmap = {}
             for char in string:
@@ -11,17 +12,15 @@ class Solution:
             mini = min(hashmap.values())
             return maxi - mini
         
-        
-        
-        
-        
-        
+        # Initialize the total sum of beauty
         total_sum = 0
+        
+        # Iterate through all possible substrings
         for i in range(len(s)):
-            for j in  range(i+1,len(s)):
+            for j in range(i+1, len(s)):
                 sub_str = s[i:j+1]
                 curr_freq = freq(sub_str)
                 total_sum += curr_freq
-        return total_sum
-                
         
+        # Return the total sum of beauty
+        return total_sum
