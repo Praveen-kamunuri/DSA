022a5810545e08d438aad700b57fc084d2419268
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-22 22:47:27 +0530

    Time: 43 ms (69.86%), Space: 16.7 MB (33.19%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -6,7 +6,8 @@
 
 class Solution:
     def deleteNode(self, node):
+        # Copy the value of the next node to the current node
         node.val = node.next.val
+        
+        # Update the next pointer of the current node to skip the next node
         node.next = node.next.next
-        
-        
