2f8fdd6858e0c27bd64f8b4a2d5a6a91aeda87f6
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-24 21:16:25 +0530

    Time: 30 ms (94.77%), Space: 16.2 MB (87.85%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -3,11 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Initialize two pointers, tortoise and rabbit, both starting at the head
         tortoise = head
         rabbit = head
+        
+        # Traverse the list with tortoise moving one step and rabbit moving two steps
         while rabbit and rabbit.next:
-            tortoise  = tortoise.next
+            tortoise = tortoise.next
             rabbit = rabbit.next.next
+        
+        # When rabbit reaches the end (or None), tortoise will be at the middle node
         return tortoise
