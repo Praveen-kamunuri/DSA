3c3ce57b7d8eea781d61f7876ae92ef0242d39bd
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-25 22:37:53 +0530

    Time: 53 ms (54.72%), Space: 20.5 MB (57.28%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,21 +6,28 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Check if the linked list is empty or has only one node
         if not head or head.next is None:
-            return None
+            return None  # No cycle
         
-        
-        
-        
+        # Initialize two pointers, rabbit and tortoise
         rabbit = head
         tortoise = head
+        
+        # Initialize an entry point for cycle detection
         entry = head
+        
+        # Use Floyd's Tortoise and Hare algorithm to detect a cycle
         while rabbit and rabbit.next:
             tortoise = tortoise.next
             rabbit = rabbit.next.next
+            
+            # When a cycle is detected, find the node where it begins
             if tortoise == rabbit:
                 while entry != tortoise:
                     entry = entry.next
                     tortoise = tortoise.next
                 return entry
-        return None
+        
+        # If no cycle is found, return None
+        return None  # No cycle
