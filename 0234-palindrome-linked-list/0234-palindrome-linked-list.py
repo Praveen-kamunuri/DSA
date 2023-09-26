990ed0507d0ee760a1f4bb3edeb0bc9afa4df0e6
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-26 23:14:03 +0530

    Time: 556 ms (61.66%), Space: 41.3 MB (71.68%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,33 +1,13 @@
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # Function to reverse a linked list in-place
-        def reverse_linked_list(node):
-            prev = None
-            current = node
-            while current:
-                next_node = current.next
-                current.next = prev
-                prev = current
-                current = next_node
-            return prev
-        
-        slow = head
-        fast = head
-        
-        # Find the middle of the linked list
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow, fast, prev = head, head, None
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        # Reverse the second half of the linked list
-        second_half = reverse_linked_list(slow)
-        
-        # Compare the first half with the reversed second half
-        first_half = head
-        while second_half:
-            if first_half.val != second_half.val:
-                return False
-            first_half = first_half.next
-            second_half = second_half.next
-        
+            slow, fast = slow.next, fast.next.next
+        prev, slow, prev.next = slow, slow.next, None
+        while slow:
+            slow.next, prev, slow = prev, slow, slow.next
+        fast, slow = head, prev
+        while slow:
+            if fast.val != slow.val: return False
+            fast, slow = fast.next, slow.next
         return True
