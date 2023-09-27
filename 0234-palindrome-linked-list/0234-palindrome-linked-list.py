b63740bd84de735b2cc418f4e141429a93caf6ce
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-27 21:54:25 +0530

    Time: 730 ms (7.62%), Space: 48.8 MB (53.87%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,13 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        slow, fast, prev = head, head, None
-        while fast and fast.next:
-            slow, fast = slow.next, fast.next.next
-        prev, slow, prev.next = slow, slow.next, None
-        while slow:
-            slow.next, prev, slow = prev, slow, slow.next
-        fast, slow = head, prev
-        while slow:
-            if fast.val != slow.val: return False
-            fast, slow = fast.next, slow.next
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        def reverse(ptr):
+            pre = None
+            nex = None
+            while ptr != None:
+                nex = ptr.next
+                ptr.next = pre
+                pre = ptr
+                ptr = nex
+            return pre
+
+    
+        if head == None or head.next == None:
+            return True
+        slow = head
+        fast = head
+        while fast.next != None and fast.next.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        slow.next = reverse(slow.next)
+        slow = slow.next
+        dummy = head
+        while slow != None:
+            if dummy.val != slow.val:
+                return False
+            dummy = dummy.next
+            slow = slow.next
         return True
+     
+
+
+
