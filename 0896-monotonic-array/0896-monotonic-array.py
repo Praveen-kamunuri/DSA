d2efe6fd5a0e8263d157c3448618d521cc074b5e
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-29 19:51:47 +0530

    Time: 870 ms (23.56%), Space: 30.4 MB (95.58%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,11 +1,13 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         n = len(nums)
-        inc , dec = True , True
-        for i in range(n-1):
-            if nums[i+1] > nums[i]:
-                dec = False
-            if nums[i+1] < nums[i]:
-                inc = False
+        inc, dec = True, True  # Initialize both flags to True
+
+        for i in range(1, n):
+            if nums[i] > nums[i-1]:
+                dec = False  # If we find an increasing pair, set dec to False
+            elif nums[i] < nums[i-1]:
+                inc = False  # If we find a decreasing pair, set inc to False
+
+        # If either flag is True, it means the list is monotonic
         return inc or dec
-        
