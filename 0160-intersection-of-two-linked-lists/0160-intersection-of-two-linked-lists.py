9b7e0fbcf1feaeb61c39e1571c35cc2f06776587
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-09-30 21:16:26 +0530

    Time: 122 ms (78.10%), Space: 31.4 MB (81.40%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,22 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # Initialize two pointers, h1 and h2, to the heads of the input linked lists.
         h1 = headA
         h2 = headB
+
+        # Iterate until h1 and h2 meet or both reach the end (None) of their respective lists.
         while h1 != h2:
+            # If h1 reaches the end of list A, move it to the head of list B.
             if h1 is None:
                 h1 = headB
             else:
+                # Otherwise, move h1 to the next node in list A.
                 h1 = h1.next
+            
+            # If h2 reaches the end of list B, move it to the head of list A.
             if h2 is None:
                 h2 = headA
             else:
+                # Otherwise, move h2 to the next node in list B.
                 h2 = h2.next
-        return h1
-            
         
+        # If h1 and h2 meet, they have found the intersection point (or reached the end, which is None).
+        # Return the intersection point (or None if there's no intersection).
+        return h1
