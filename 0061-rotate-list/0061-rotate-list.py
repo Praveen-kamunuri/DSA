47c6729c0f12e05b51dc1af28a12ec51c63616bb
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-04 23:03:26 +0530

    Time: 52 ms (8.80%), Space: 16.2 MB (69.23%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -3,27 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Check if the linked list is empty
         if not head:
             return None
         
+        # Find the length of the linked list
         current = head
         length = 1
         while current.next is not None:
             length += 1
             current = current.next
+        
+        # Make the linked list circular by connecting the last node to the head
         current.next = head
         
-        k = k % length 
-        dest = ( length - k )
+        # Calculate the effective rotation value (k % length)
+        k = k % length
+        
+        # Calculate the destination position where rotation ends
+        dest = (length - k)
+        
+        # Traverse the linked list to the destination position
         cnt = 1
         current = head
         while cnt != dest:
             cnt += 1
             current = current.next
-        head  = current.next
+        
+        # Update the head to the new rotated position
+        head = current.next
+        
+        # Set the current node's next to None to break the circular connection
         current.next = None
+        
+        # Return the new head of the rotated linked list
         return head
-        
-        
