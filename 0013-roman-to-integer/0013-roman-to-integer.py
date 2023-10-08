b9a77ed64f90e2095f539022117d72e385b3ff5b
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-08 20:13:29 +0530

    Time: 48 ms (74.85%), Space: 16.4 MB (32.30%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,23 +1,26 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         hashmap = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000,
+            'I'      :       1,
+            'V'      :       5,
+            'X'      :       10,
+            'L'      :       50,
+            'C'      :       100,
+            'D'      :       500,
+            'M'      :       1000,
+
         }
-
-        pre_val = 0  # Corrected variable name
+        
+        
+        pre_val = 0
         res = 0
-
+        
         for i in s[::-1]:
             if hashmap[i] >= pre_val:
                 res += hashmap[i]
             else:
                 res -= hashmap[i]
-
-            pre_val = hashmap[i]  # Corrected variable name
+            
+            pre_val = hashmap[i]
         return res
+        
