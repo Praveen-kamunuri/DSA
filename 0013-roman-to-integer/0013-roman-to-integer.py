ce01264abfb61ff4ddd9197b9c94c0af344b0294
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-08 20:12:57 +0530

    Time: 46 ms (83.50%), Space: 16.1 MB (90.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,5 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        # Create a dictionary (hashmap) to map Roman numerals to their integer values.
         hashmap = {
             'I': 1,
             'V': 5,
@@ -10,16 +9,15 @@ class Solution:
             'D': 500,
             'M': 1000,
         }
-        
-        res = 0          # Initialize the result variable to store the integer value.
-        prev_val = 0     # Initialize a variable to keep track of the previous Roman numeral value.
-        
-        # Iterate through the input string 's' in reverse order.
-        for i in reversed(s):
-            if hashmap[i] >= prev_val:
-                res += hashmap[i]   # If the current value is greater or equal to the previous, add it to the result.
+
+        pre_val = 0  # Corrected variable name
+        res = 0
+
+        for i in s[::-1]:
+            if hashmap[i] >= pre_val:
+                res += hashmap[i]
             else:
-                res -= hashmap[i]   # If the current value is smaller than the previous, subtract it from the result.
-            prev_val = hashmap[i]   # Update the previous value for the next iteration.
-        
-        return res   # Return the final integer result.
+                res -= hashmap[i]
+
+            pre_val = hashmap[i]  # Corrected variable name
+        return res
