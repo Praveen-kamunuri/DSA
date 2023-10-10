5fda3467f1ca93555d6a9169c59aac56da737afd
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-10 22:35:48 +0530

    Time: 51 ms (7.71%), Space: 16.1 MB (98.07%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,28 +1,30 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()  # Remove leading and trailing whitespace
+        s = s.strip()
         if not s:
-            return 0  # If the string is empty, return 0
-
-        sign = 1  # Initialize the sign to positive
+            return 0
+        
+        sign = 1
         if s[0] == '-':
             sign = -1
             s = s[1:]
+            
         elif s[0] == '+':
             s = s[1:]
-
+        
         result = 0
         for char in s:
             if char.isdigit():
                 result = result * 10 + int(char)
             else:
-                break  # Stop parsing when a non-digit character is encountered
-
-        # Apply sign and handle overflow
-        result = sign * result
-        if result < -2**31:
-            return -2**31
-        elif result > 2**31 - 1:
-            return 2**31 - 1
+                break
+            
+        result = result * sign
+        
+        if result < -2 ** 31:
+            return -2 ** 31
+        elif result > 2 ** 31 -1:
+            return 2 ** 31 -1
         else:
             return result
+                
