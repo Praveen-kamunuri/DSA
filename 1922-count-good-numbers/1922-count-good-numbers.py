5f0b84edecd068d96d05eca4107f37e8c839caca
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-10 23:43:42 +0530

    Time: 38 ms (63.59%), Space: 16.1 MB (82.51%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,21 +1,19 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         
-        def power(base,exp):
+        def power(base, exp):
             res = 1
             while exp > 0:
                 if exp % 2 == 0:
-                    base = (base * base) % ( 10 ** 9 + 7)
+                    base = (base * base) % (10 ** 9 + 7)  # Square base and apply modulo if exp is even
                     exp = exp // 2
                 else:
-                    res = (res * base) % (10 ** 9 + 7)
+                    res = (res * base) % (10 ** 9 + 7)  # Multiply res by base and apply modulo if exp is odd
                     exp = exp - 1
             return res
         
+        odd = n // 2  # Calculate the count of odd digits
+        even = n - odd  # Calculate the count of even digits
         
-        
-        odd = n // 2
-        even = n - odd
-        
-        ans = ( power(5 , even) * power(4 ,odd) )   % ( 10 ** 9 + 7)
-        return ans
+        ans = (power(5, even) * power(4, odd)) % (10 ** 9 + 7)  # Calculate the count of "good numbers" with modulo
+        return ans  # Return the result
