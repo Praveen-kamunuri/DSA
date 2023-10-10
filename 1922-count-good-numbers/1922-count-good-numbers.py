05321a6786a667bbab638fb3355441b617205f29
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-10 23:35:52 +0530

    Time: 42 ms (38.53%), Space: 16.2 MB (57.68%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        def power(base, powerr):
+            ans = 1
+            while powerr > 0:
+                if powerr % 2 == 0:
+                    base = (base * base) % (10**9 + 7)
+                    powerr = powerr // 2
+                else:
+                    ans = (ans * base) % (10**9 + 7)
+                    powerr = powerr - 1
+            return ans
+
+        odd = n // 2
+        even = n - odd
+
+        ans = (power(5, even) * power(4, odd)) % (10**9 + 7)
+        return ans
