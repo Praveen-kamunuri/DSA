5f2e84aeb032833fe3cea9d488d80cb704b3c9e7
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-14 22:59:17 +0530

    Time: 44 ms (31.95%), Space: 16.3 MB (97.73%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -7,9 +7,7 @@ class Solution:
         for i in range(1 << n):
             sub = []
             for j in range(n):
-                
-                
-                if ( (1<<j) & i ):
+                if  ( ( 1 << j) & i ):
                     sub.append(nums[j])
             ans.append(sub)
         return ans
