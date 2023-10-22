b51314372f31385d20445134cd33c33e596802aa
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-22 20:58:13 +0530

    Time: 28 ms (97.28%), Space: 16.3 MB (34.32%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,22 +1,25 @@
 class Solution:
     def checkString(self, s: str) -> bool:
         n = len(s)
-        if  'b' in s:
-            start_ind = 0
-            for  i in range(n):
+        
+        if 'b' in s:
+            start_ind = 0  # Initialize the start index
+            
+            # Find the first occurrence of 'b' and set it as the start index
+            for i in range(n):
                 if s[i] != 'b':
                     continue
                 else:
                     start_ind = i
                     break
-            for i in range(start_ind,n):
+            
+            # Check if there is any 'a' after the start index; if yes, return False
+            for i in range(start_ind, n):
                 if s[i] == 'a':
                     return False
+            
+            # If there are no 'a' characters after the first 'b', return True
             return True
         else:
+            # If there are no 'b' characters in the string, return True
             return True
-            
-        
-        
-
-        
