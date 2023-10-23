1d1f119bbc19ee815bb4eb3f2516493874538ab1
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-23 11:20:59 +0530

    Time: 129 ms (42.30%), Space: 17.5 MB (86.20%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,20 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # Get the length of the input list `nums`
         n = len(nums)
-        
-        # Sort the list in ascending order
-        nums.sort()
-        
-        # Iterate through the list
-        for i in range(n):
-            # Check if the current element is equal to its index (i)
-            if nums[i] == i:
-                # If they are equal, continue to the next iteration to look for the missing number
-                continue
-            else:
-                # If the current element is not equal to its index, return the index as the missing number
-                return i
-        
-        # If all elements are in order, the missing number is n (the last index + 1)
-        return n
+        summ = n * (n+1) // 2
+        s = 0
+        for i in nums:
+            s += i
+        return summ - s
+            
