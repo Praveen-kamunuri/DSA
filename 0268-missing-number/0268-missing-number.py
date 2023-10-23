35d1dc33f83d597d6fde37fc68396a975f76b58d
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-23 11:21:55 +0530

    Time: 114 ms (94.98%), Space: 17.6 MB (60.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # Get the length of the input list `nums`
         n = len(nums)
+        
+        # Calculate the expected sum of numbers from 0 to n using the formula for the sum of an arithmetic series
         summ = n * (n+1) // 2
+        
+        # Initialize a variable `s` to keep track of the sum of elements in the list
         s = 0
+        
+        # Iterate through the elements in the list
         for i in nums:
-            s += i
+            s += i  # Add each element to the sum
+        
+        # Calculate and return the missing number by subtracting the actual sum from the expected sum
         return summ - s
-            
