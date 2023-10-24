01bfbd7345b0090024ef094e21d444cf9c799f89
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-24 12:56:50 +0530

    Time: 165 ms (9.43%), Space: 17.8 MB (42.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        hashmap = {}  # Initialize an empty hashmap to store element counts
-        target = len(nums) // 2  # Calculate the target count required for majority
-
-        # Iterate over each element in the nums list
-        for ele in nums:
-            if ele in hashmap:  # If element already exists in hashmap
-                hashmap[ele] += 1  # Increment its count by 1
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        maj = n // 2
+        hashmap = {}
+        for i in nums:
+            if i in hashmap:
+                hashmap[i] += 1
             else:
-                hashmap[ele] = 1  # Add the element to hashmap with count 1
-
-        # Iterate over each key-value pair in the hashmap
-        for ele, value in hashmap.items():
-            if value > target:  # If the count is greater than or equal to the target
-                return ele  # Return the majority element
-
+                hashmap[i] = 1
+        for key , val in hashmap.items():
+            if val > maj:
+                return key
+            
