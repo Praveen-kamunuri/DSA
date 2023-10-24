8354d4312c2a96349e7c0c74dfd6bb64a766ab57
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-24 11:49:44 +0530

    Time: 68 ms (55.12%), Space: 17.6 MB (22.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Create an empty dictionary to store the numbers in the list and their indices
         hashmap = {}
-        for i , num in enumerate(nums):
+        
+        # Loop through the list, keeping track of both the number and its index
+        for i, num in enumerate(nums):
+            # Calculate the complement needed to reach the target
             complement = target - num
+            
+            # Check if the complement is in the hashmap and the indices are different
             if complement in hashmap and hashmap[complement] != i:
-                return i , hashmap[complement]
+                # If a pair is found, return their indices
+                return [hashmap[complement], i]
+            
+            # Add the current number and its index to the hashmap
             hashmap[num] = i
+        
+        # If no solution is found, return an empty list
         return []
