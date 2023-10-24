ee0d4e1565f0496b5863787176bd014613c49dca
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-24 19:37:30 +0530

    Time: 891 ms (27.27%), Space: 27.3 MB (73.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Get the number of days (the length of the prices list)
         n = len(prices)
+        
+        # Initialize the profit to 0 and the minimum price to the price on the first day
         profit = 0
         mini = prices[0]
-        for i in range(1,n):
+        
+        # Loop through the prices starting from the second day (index 1)
+        for i in range(1, n):
+            # Calculate the profit (current price - minimum price so far)
             cost = prices[i] - mini
-            profit = max(profit , cost)
-            mini = min(mini , prices[i])
-        return profit
             
+            # Update the maximum profit if the current profit is greater
+            profit = max(profit, cost)
+            
+            # Update the minimum price if the current price is lower
+            mini = min(mini, prices[i])
+        
+        # Return the maximum profit achievable
+        return profit
