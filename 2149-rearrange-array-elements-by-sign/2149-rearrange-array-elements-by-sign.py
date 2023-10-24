2b6b23badfd496442d6dee628192d6e60fcf4afb
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-24 19:49:01 +0530

    Time: 1171 ms (82.86%), Space: 47 MB (53.66%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,34 +1,14 @@
-class Solution(object):
-    def rearrangeArray(self, nums):
-        # Create a hash map to group positive and negative elements
-        hashmap = {'pos': [], 'neg': []}
-
-        # Group positive and negative elements in the hash map
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                hashmap['pos'].append(nums[i])
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [None] * n
+        positive_ind = 0
+        negative_ind = 1
+        for i in range(n):
+            if nums[i] < 0:
+                ans[negative_ind] = nums[i]
+                negative_ind += 2
             else:
-                hashmap['neg'].append(nums[i])
-
-        # Create an empty list to store the rearranged elements
-        li = []
-
-        # Get the lengths of the 'pos' and 'neg' lists
-        pos_len = len(hashmap['pos'])
-        neg_len = len(hashmap['neg'])
-
-        # Determine the maximum length between 'pos' and 'neg'
-        max_len = max(pos_len, neg_len)
-
-        # Iterate through the range of max_len
-        for i in range(max_len):
-            # Append elements from 'pos' to the list 'li' if the index is within bounds
-            if i < pos_len:
-                li.append(hashmap['pos'][i])
-
-            # Append elements from 'neg' to the list 'li' if the index is within bounds
-            if i < neg_len:
-                li.append(hashmap['neg'][i])
-
-        # Return the rearranged list
-        return li
+                ans[positive_ind] = nums[i]
+                positive_ind += 2
+        return ans
