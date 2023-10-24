beecb7150999e068dc6578ee85da93afca480162
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-24 11:56:57 +0530

    Time: 50 ms (6.44%), Space: 16.2 MB (78.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,24 +1,8 @@
-class Solution(object):
-    def sortColors(self, nums):
-        cont0 = 0
-        cont1 = 0
-        cont2 = 0
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1 , n):
+                if nums[j] < nums[i]:
+                    nums[i] , nums[j] = nums[j] , nums[i]
         
-        # Iterate over the input array to count the occurrences of each color
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                cont0 += 1
-            elif nums[i] == 1:
-                cont1 += 1
-            elif nums[i] == 2:
-                cont2 += 1
-        
-        # Update the input array to sort the colors
-        for i in range(cont0):
-            nums[i] = 0
-        for i in range(cont0, cont0 + cont1):
-            nums[i] = 1
-        for i in range(cont0 + cont1, cont0 + cont1 + cont2):
-            nums[i] = 2
-        
-        return nums
