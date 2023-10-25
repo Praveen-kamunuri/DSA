ecdf6edab43527409216239988cbf4f257849ac0
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-25 11:26:24 +0530

    Time: 51 ms (29.74%), Space: 16.3 MB (18.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,24 +1,18 @@
-class Solution(object):
-    def nextPermutation(self, nums):
-        # Find the first index from the right where nums[i] < nums[i+1]
-        i = len(nums) - 2
-        while i >= 0 and nums[i] >= nums[i+1]:
-            i -= 1
-    
-        if i >= 0:
-            # Find the first index from the right where nums[j] > nums[i]
-            j = len(nums) - 1
-            while j >= 0 and nums[j] <= nums[i]:
-                j -= 1
-            # Swap nums[i] and nums[j]
-            nums[i], nums[j] = nums[j], nums[i]
-    
-        # Reverse the suffix starting from i+1
-        left = i + 1
-        right = len(nums) - 1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
-
-    
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        ind = -1
+        for i in range(n-2,-1,-1):
+            if nums[i] < nums[i+1]:
+                ind = i
+                break
+        if ind == -1:
+            nums.reverse()
+            return nums
+        for i in range(n-1,ind,-1):
+            if nums[i] > nums[ind]:
+                nums[i] , nums[ind] = nums[ind] , nums[i]
+                break
+        
+        nums[ind+1:] = nums[:ind:-1]
+        return nums
