04cbe852dacf6255b480439501764ba4f3dd3c8c
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-25 11:48:56 +0530

    Time: 45 ms (73.31%), Space: 16.1 MB (81.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,18 +1,28 @@
+
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         n = len(nums)
         ind = -1
-        for i in range(n-2,-1,-1):
-            if nums[i] < nums[i+1]:
+
+        # Step 1: Find the first element from the right that is smaller than the next element.
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
                 ind = i
                 break
+
+        # Step 2: If no such element exists, reverse the entire list.
         if ind == -1:
             nums.reverse()
-            return nums
-        for i in range(n-1,ind,-1):
+            return nums  # This line may not be needed as the list is reversed in-place.
+
+        # Step 3: Find the smallest element to the right of `ind` that is greater than `nums[ind]`.
+        for i in range(n - 1, ind, -1):
             if nums[i] > nums[ind]:
-                nums[i] , nums[ind] = nums[ind] , nums[i]
+                # Swap the two elements.
+                nums[i], nums[ind] = nums[ind], nums[i]
                 break
-        nums[ind+1:] = reversed(nums[ind+1:])
-        return nums
-        
+
+        # Step 4: Reverse the subarray to the right of `ind`.
+        nums[ind + 1:] = reversed(nums[ind + 1:])
+
+        return nums  # Return the modified list.
