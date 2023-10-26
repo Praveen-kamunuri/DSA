48756279a8060ce3b718c2633b3fa9d603297471
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-26 11:37:37 +0530

    Time: 35 ms (91.58%), Space: 16.2 MB (63.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,25 +1,12 @@
-class Solution(object):
-    def rotate(self, matrix):
-        rows = len(matrix)# Calculate the number of rows in the input matrix and assign it to the variable "rows".
-        res = []
-        # Initialize an empty list "res" which will be used to store the rotated matrix.
-
-        for i in range(rows):
-            # Start a loop that iterates over each row of the input matrix.
-
-            temp = []
-            # Initialize an empty list "temp" which will be used to store the elements of the current row after the rotation.
-
-            for j in range(rows):
-                # Start a nested loop that iterates over each element in the current row.
-
-                temp.insert(0, matrix[j][i])
-                # Insert the element at position [j][i] (row j, column i) of the original matrix into the "temp" list at the beginning.
-                # This effectively transposes the matrix (interchanging rows and columns) as it traverses each row.
-
-            res.append(temp)
-            # Append the "temp" list (which represents the transposed row) to the "res" list.
-            # This creates a transposed version of the entire matrix.
-
-        for i in range(rows):
-            matrix[i] = res[i]  # Copy the i-th row from the transposed matrix (res) back to the original matrix (matrix).
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+        
+                
+        
