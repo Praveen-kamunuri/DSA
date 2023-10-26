0b44247666f8547bdea7c3e75f277d38476662ed
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-26 11:39:40 +0530

    Time: 37 ms (86.00%), Space: 16.3 MB (63.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,12 +1,12 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix)
-        
+        n = len(matrix)  # Get the size of the matrix
+
+        # Transpose the matrix (swap elements across the main diagonal)
         for i in range(n):
             for j in range(i):
-                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse each row to complete the 90-degree rotation
         for i in range(n):
             matrix[i].reverse()
-        
-                
-        
