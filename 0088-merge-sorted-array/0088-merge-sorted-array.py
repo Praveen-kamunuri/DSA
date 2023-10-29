bf57086b087e80257dff252bfd8dcc1588d13020
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-29 22:20:22 +0530

    Time: 39 ms (81.98%), Space: 16.2 MB (63.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        start_ind = m
-        end_ind = len(nums1) - 1
-        j = 0
+        start_ind = m  # Initialize a pointer for the start of the portion to merge in nums1
+        end_ind = len(nums1) - 1  # Calculate the end index of the nums1 array
+        j = 0  # Initialize a pointer for nums2
+
+        # Iterate through nums1 and nums2
         while start_ind < len(nums1) and j != n:
-            nums1[start_ind] = nums2[j]
+            nums1[start_ind] = nums2[j]  # Append nums2 element to nums1
             start_ind += 1
             j += 1
-        nums1.sort()
-        return nums1
-            
-        
+
+        nums1.sort()  # Sort the merged nums1 array (this could be optimized)
+
+        return nums1  # Return the merged and sorted nums1 array
