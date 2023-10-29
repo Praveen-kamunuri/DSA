65f2ea292270ed01885062882cf3982a150deb71
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-29 22:45:13 +0530

    Time: 57 ms (45.78%), Space: 17.7 MB (74.90%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -1,9 +1,13 @@
 class Solution:
     def largestOddNumber(self, num: str) -> str:
         n = len(num)
-        for i in range(n-1,-1,-1):
+        
+        # Iterate through the characters of the string in reverse order
+        for i in range(n-1, -1, -1):
+            # Check if the integer value of the character at position i is odd
             if int(num[i]) % 2 == 1:
+                # If an odd digit is found, update 'num' to keep only the prefix up to and including this digit
                 num = num[0:i+1]
-                return num
-                
-        return ""
+                return num  # Return the updated 'num'
+        
+        return ""  # If there are no odd digits, return an empty string
