b8f47e81edc412787b4d09ee7c84abe01d534ea8
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-31 01:05:55 +0530

    Time: 86 ms (44.40%), Space: 17.7 MB (72.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,41 @@
-class Solution(object):
-    def start_ind(self, nums, target):
+class Solution:
+    def lowerBound(self,nums,target):
         n = len(nums)
         low = 0
         high = n - 1
-        first = -1  # Initialize the index for the first occurrence of the target
+        low_bound = -1
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
-                first = mid  # Update the first occurrence index and continue searching on the left side
+                low_bound = mid
                 high = mid - 1
             elif nums[mid] < target:
                 low = mid + 1
             else:
                 high = mid - 1
-        return first
+        return low_bound
     
-    def end_ind(self, nums, target):
+    def upperBound(self,nums,target):
         n = len(nums)
         low = 0
-        high = n - 1
-        end = -1  # Initialize the index for the last occurrence of the target
+        high = n -1
+        up_bound = -1
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
-                end = mid  # Update the last occurrence index and continue searching on the right side
+                up_bound = mid
                 low = mid + 1
             elif nums[mid] < target:
                 low = mid + 1
             else:
                 high = mid - 1
-        return end
-                
-                
-    def searchRange(self, nums, target):
-        start = self.start_ind(nums, target)  # Get the index of the first occurrence of the target
-        end = self.end_ind(nums, target)  # Get the index of the last occurrence of the target
-        return [start, end]  # Return the indices of the target's occurrences
+        return up_bound
+    
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first = self.lowerBound(nums,target)
+        second = self.upperBound(nums,target)
+        return [first,second]
+            
+        
+        
