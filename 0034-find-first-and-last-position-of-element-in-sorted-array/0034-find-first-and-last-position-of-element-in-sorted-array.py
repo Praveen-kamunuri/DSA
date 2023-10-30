a699bf2350900739a151107950b647551f12da26
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-31 01:06:30 +0530

    Time: 86 ms (44.40%), Space: 17.7 MB (72.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,47 @@
 class Solution:
-    def lowerBound(self,nums,target):
-        n = len(nums)
-        low = 0
-        high = n - 1
-        low_bound = -1
+    def lowerBound(self, nums, target):
+        n = len(nums)         # Get the length of the input 'nums' list.
+        low = 0               # Initialize a low pointer to the beginning of the list.
+        high = n - 1          # Initialize a high pointer to the end of the list.
+        low_bound = -1        # Initialize 'low_bound' to -1 (default if the target is not found).
+
         while low <= high:
-            mid = (low + high) // 2
+            mid = (low + high) // 2   # Calculate the middle index.
+
             if nums[mid] == target:
-                low_bound = mid
+                low_bound = mid       # If the middle element is equal to the target, update 'low_bound' and continue searching the left half.
                 high = mid - 1
+
             elif nums[mid] < target:
-                low = mid + 1
+                low = mid + 1          # If the middle element is less than the target, update 'low' to search in the right half.
+
             else:
-                high = mid - 1
+                high = mid - 1         # If the middle element is greater than the target, update 'high' to search in the left half.
+
         return low_bound
-    
-    def upperBound(self,nums,target):
-        n = len(nums)
-        low = 0
-        high = n -1
-        up_bound = -1
+
+    def upperBound(self, nums, target):
+        n = len(nums)         # Get the length of the input 'nums' list.
+        low = 0               # Initialize a low pointer to the beginning of the list.
+        high = n - 1          # Initialize a high pointer to the end of the list.
+        up_bound = -1         # Initialize 'up_bound' to -1 (default if the target is not found).
+
         while low <= high:
-            mid = (low + high) // 2
+            mid = (low + high) // 2   # Calculate the middle index.
+
             if nums[mid] == target:
-                up_bound = mid
+                up_bound = mid        # If the middle element is equal to the target, update 'up_bound' and continue searching the right half.
                 low = mid + 1
+
             elif nums[mid] < target:
-                low = mid + 1
+                low = mid + 1          # If the middle element is less than the target, update 'low' to search in the right half.
+
             else:
-                high = mid - 1
+                high = mid - 1         # If the middle element is greater than the target, update 'high' to search in the left half.
+
         return up_bound
-    
-    
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = self.lowerBound(nums,target)
-        second = self.upperBound(nums,target)
-        return [first,second]
-            
-        
-        
+
+    def searchRange(self, nums, target):
+        first = self.lowerBound(nums, target)   # Find the lower bound of the target.
+        second = self.upperBound(nums, target)  # Find the upper bound of the target.
+        return [first, second]  # Return a list containing the lower and upper bounds.
