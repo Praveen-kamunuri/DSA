29618158f771d396c2655dbfd68432e617d417ee
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-10-31 22:08:35 +0530

    Time: 40 ms (87.34%), Space: 16.5 MB (85.14%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,38 +1,25 @@
-# Define a class named Solution
-class Solution(object):
-    # Define a method named isIsomorphic that takes two strings s and t as input
-    def isIsomorphic(self, s, t):
-        # Check if the lengths of the input strings s and t are not equal
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t):
-            # If the lengths are different, return False since they cannot be isomorphic
             return False
-        
-        # Initialize an empty dictionary s_hashmap to store mappings from s to t
-        s_hashmap = {}
-        # Initialize an empty dictionary t_hashmap to store mappings from t to s
-        t_hashmap = {}
-        
-        # Iterate through pairs of characters from s and t using the zip function
-        for i, j in zip(s, t):
-            # Check if the character i from s is already in s_hashmap
-            if i in s_hashmap:
-                # If it is, check if the mapping in s_hashmap for i is not equal to j
-                if s_hashmap[i] != j:
-                    # If the mapping is invalid, return False
+
+        s_map = {}  # Mapping from s to t
+        t_map = {}  # Mapping from t to s
+
+        for i in range(len(s)):
+            char_s = s[i]
+            char_t = t[i]
+
+            if char_s in s_map:
+                if s_map[char_s] != char_t:
                     return False
             else:
-                # If i is not in s_hashmap, add a mapping from i to j in s_hashmap
-                s_hashmap[i] = j
-                
-            # Check if the character j from t is already in t_hashmap
-            if j in t_hashmap:
-                # If it is, check if the mapping in t_hashmap for j is not equal to i
-                if t_hashmap[j] != i:
-                    # If the mapping is invalid, return False
+                s_map[char_s] = char_t
+
+            if char_t in t_map:
+                if t_map[char_t] != char_s:
                     return False
             else:
-                # If j is not in t_hashmap, add a mapping from j to i in t_hashmap
-                t_hashmap[j] = i
-        
-        # If the loop completes without any invalid mappings, return True, indicating isomorphism
+                t_map[char_t] = char_s
+
         return True
