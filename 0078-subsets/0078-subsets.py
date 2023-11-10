8161d8515edcc7f7027c02b98b14933becf86123
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-11-10 20:39:08 +0530

    Time: 35 ms (88.90%), Space: 16.4 MB (78.66%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []  # Initialize an empty list to store the subsets
-        
-        n = len(nums)  # Calculate the length of the input list
-        
-        for i in range(1 << n):  # Iterate through all possible subsets (2^n iterations)
-            sub = []  # Initialize an empty list to store the current subset
-            for j in range(n):  # Iterate through the elements of the input list
-                # Check if the j-th bit of the binary representation of 'i' is set
-                if (1 << j) & i:
-                    sub.append(nums[j])  # If the bit is set, add the corresponding element to the subset
-            ans.append(sub)  # Add the current subset to the list of subsets
-        return ans  # Return the list of all subsets
+        n = len(nums)
+        res = []
+        for i in range(2**n):
+            subset = []
+            for j in range(0,n):
+                if i & (1 << j):
+                    subset.append(nums[j])
+            res.append(subset)
+        return res
