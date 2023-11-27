5a4c306fda3b4c3b8f91d8e8de40a72a8166df10
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-11-27 12:33:20 +0530

    Time: 63 ms (50.84%), Space: 16.5 MB (31.99%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,17 +2,22 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         ds = []
-
-
-        def findCombination(ind: int, target: int):
+        
+        def findCombination(ind , target):
             if ind == len(candidates):
                 if target == 0:
                     ans.append(ds[:])
                 return
+            
             if candidates[ind] <= target:
                 ds.append(candidates[ind])
                 findCombination(ind, target - candidates[ind])
                 ds.pop()
-            findCombination(ind + 1, target)
-        findCombination(0, target)
+            findCombination(ind + 1 , target)
+            return ans
+        
+        
+        
+        
+        findCombination(0,target)
         return ans
