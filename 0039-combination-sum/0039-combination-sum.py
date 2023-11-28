8707ec741a163f688a94b9175e5ea0f008081cbf
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-11-28 22:08:48 +0530

    Time: 57 ms (74.41%), Space: 16.5 MB (11.04%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,25 +1,34 @@
+from typing import List
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # List to store the final combinations
         ans = []
+        # List to store the current combination being explored
         ds = []
-        
-        def findCombination(ind , target):
+
+        # Function to recursively find combinations
+        def findCombination(ind, target):
+            # Base case: if index reaches the end of candidates
             if ind == len(candidates):
+                # Check if the target is achieved, and if so, append the current combination to ans
                 if target == 0:
                     ans.append(ds[:])
                 return
+
+            # Check if the current candidate can be a part of the combination
             if candidates[ind] <= target:
+                # Include the current candidate in the combination
                 ds.append(candidates[ind])
-                findCombination(ind , target - candidates[ind])
+                # Recursively call the function with updated index and target
+                findCombination(ind, target - candidates[ind])
+                # Backtrack by removing the last added candidate to explore other possibilities
                 ds.pop()
-            findCombination(ind + 1 , target)
-            
-            
-            
-            
-            
-            
-        findCombination(0,target)
+
+            # Move on to the next candidate
+            findCombination(ind + 1, target)
+
+        # Start the recursive search from the beginning
+        findCombination(0, target)
+        # Return the final list of combinations
         return ans
-        
-        
