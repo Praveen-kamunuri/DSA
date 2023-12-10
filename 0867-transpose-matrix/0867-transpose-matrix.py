7527231676b0937938cf1a9c35d874742d3802d2
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-12-10 13:27:06 +0530

    Time: 77 ms (36.27%), Space: 17.2 MB (53.19%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,9 +1,22 @@
+from typing import List
+
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        # Get the number of rows and columns in the original matrix
         rows = len(matrix)
         cols = len(matrix[0])
-        res = [ [0] * rows for i in range(cols) ]
+        
+        # Initialize the result matrix with dimensions swapped (cols x rows)
+        res = [[0] * rows for i in range(cols)]
+        
+        # Iterate through each element of the original matrix
         for i in range(rows):
             for j in range(cols):
+                # Transpose the element from the original matrix to the result matrix
+                # Swap the indices to achieve the transpose effect
                 res[j][i] = matrix[i][j]
+        
+        # Return the transposed matrix
         return res
+
+    
