875e27da4efa3a2cf89a40497a02dacd316b3b83
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-12-11 21:18:00 +0530

    Time: 97 ms (37.78%), Space: 17.9 MB (30.41%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -1,13 +1,22 @@
+from typing import List
+
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
+        # Initialize a hashmap to store the count of each element in the array
         hashmap = {}
+        
+        # Count occurrences of each element in the array
         for i in arr:
             if i in hashmap:
                 hashmap[i] += 1
             else:
                 hashmap[i] = 1
                 
+        # Calculate the minimum count required for an element to be considered "special"
         element_with_percentage = (1 / 4) * len(arr)
-        for key , val in hashmap.items():
+        
+        # Iterate through the hashmap to find the "special" element
+        for key, val in hashmap.items():
             if val > element_with_percentage:
+                # Return the first "special" element found
                 return key
