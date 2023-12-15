fea6d8b6f5e1ae71241cf87fe18234b5e2d1c9d8
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-12-15 20:00:11 +0530

    Time: 60 ms (63.28%), Space: 16.3 MB (91.39%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,34 +1,34 @@
-from typing import List
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # List to store the final combinations
         ans = []
-        # List to store the current combination being explored
         ds = []
-
-        # Function to recursively find combinations
-        def findCombination(ind, target):
-            # Base case: if index reaches the end of candidates
+        
+        def findCombinations(ind , target):
             if ind == len(candidates):
-                # Check if the target is achieved, and if so, append the current combination to ans
                 if target == 0:
                     ans.append(ds[:])
-                return
-
-            # Check if the current candidate can be a part of the combination
+                return 
+            
             if candidates[ind] <= target:
-                # Include the current candidate in the combination
                 ds.append(candidates[ind])
-                # Recursively call the function with updated index and target
-                findCombination(ind, target - candidates[ind])
-                # Backtrack by removing the last added candidate to explore other possibilities
+                findCombinations(ind , (target - candidates[ind]) )
                 ds.pop()
-
-            # Move on to the next candidate
-            findCombination(ind + 1, target)
-
-        # Start the recursive search from the beginning
-        findCombination(0, target)
-        # Return the final list of combinations
+                
+            findCombinations(ind + 1 , target)
+                
+        findCombinations(0 , target)
         return ans
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        findCombinations(0 , target)
+        
+        
+        
