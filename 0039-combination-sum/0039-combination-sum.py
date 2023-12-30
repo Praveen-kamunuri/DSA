de435aa2f29fe8e4df12ea2220c13954378dcc69
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-12-30 20:30:49 +0530

    Time: 110 ms (11.16%), Space: 17.5 MB (7.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,32 +1,22 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # Initialize an empty list to store the final combinations
         ans = []
-        # Initialize an empty list to store the current combination being formed
         ds = []
-
-        # Define a recursive function to find combinations
-        def findCombinations(ind, target):
-            # Base case: If the index reaches the end of the candidates list
+        
+        def findCombinations(ind , target):
             if ind == len(candidates):
-                # If the target is reduced to 0, add the current combination to the result
                 if target == 0:
                     ans.append(ds[:])
                 return
-
-            # Check if the current candidate can be included in the combination
+        
             if candidates[ind] <= target:
-                # Include the current candidate in the combination
                 ds.append(candidates[ind])
-                # Recursively call the function with updated index and target
-                findCombinations(ind, (target - candidates[ind]))
-                # Remove the last element to backtrack and explore other possibilities
+                findCombinations(ind  , target - candidates[ind])
                 ds.pop()
-
-            # Move to the next candidate in the list
-            findCombinations(ind + 1, target)
-
-        # Start the recursive function with initial index and target
-        findCombinations(0, target)
-        # Return the final list of combinations
+                
+            
+            findCombinations(ind + 1 , target)
+            
+        findCombinations(0 , target)
         return ans
+        
