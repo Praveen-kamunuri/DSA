4db430763a169f165fbe9c5554e6949c1d3803a2
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2023-12-30 20:31:01 +0530

    Time: 62 ms (52.66%), Space: 17.4 MB (12.33%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,32 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # Initialize an empty list to store the final combinations
         ans = []
+        # Initialize an empty list to store the current combination being formed
         ds = []
-        
-        def findCombinations(ind , target):
+
+        # Define a recursive function to find combinations
+        def findCombinations(ind, target):
+            # Base case: If the index reaches the end of the candidates list
             if ind == len(candidates):
+                # If the target is reduced to 0, add the current combination to the result
                 if target == 0:
                     ans.append(ds[:])
                 return
-        
+
+            # Check if the current candidate can be included in the combination
             if candidates[ind] <= target:
+                # Include the current candidate in the combination
                 ds.append(candidates[ind])
-                findCombinations(ind  , target - candidates[ind])
+                # Recursively call the function with updated index and target
+                findCombinations(ind, (target - candidates[ind]))
+                # Remove the last element to backtrack and explore other possibilities
                 ds.pop()
-                
-            
-            findCombinations(ind + 1 , target)
-            
-        findCombinations(0 , target)
+
+            # Move to the next candidate in the list
+            findCombinations(ind + 1, target)
+
+        # Start the recursive function with initial index and target
+        findCombinations(0, target)
+        # Return the final list of combinations
         return ans
-        
