ecf99a8e4d807ba28949de59de52dab6ca528873
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-11 20:25:29 +0530

    Time: 45 ms (28.49%), Space: 17.4 MB (19.24%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+        ds = []
+        
+        
+        def findComb(num,summ,size):
+            if size == k:
+                if summ == n:
+                    res.append(ds[:])
+                return
+            if num > 9:
+                return
+            
+            ds.append(num)
+            findComb(num + 1,summ + num,size + 1)
+            ds.pop()
+            findComb(num + 1,summ,size)
+            
+            
+        findComb(1,0,0)
+        return res
+        
