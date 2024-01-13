f123dfab5f68bc715848fe966e4ec5f7d847cbde
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-13 20:40:45 +0530

    Time: 30 ms (94.29%), Space: 17.3 MB (20.60%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,44 +1,45 @@
+from typing import List
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # Check if the input digits is empty
         if not digits:
             return []
-        
 
+        # Define a mapping of digits to letters
         hashmap = {
-            
-            
-            '2' : 'abc',
-            '3' : 'def',
-            '4' : 'ghi',
-            '5' : 'jkl',
-            '6' : 'mno',
-            '7' : 'pqrs',
-            '8' : 'tuv',
-            '9' : 'wxyz',
-            
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
         }
-        
-        
-        def backtrack(ind , path):
-            if ind == len(digits):
+
+        # Helper function to generate combinations using backtracking
+        def backtrack(index, path):
+            # If the current combination is complete, add it to the result
+            if index == len(digits):
                 res.append(''.join(path))
                 return
-            
-            curr_letter = digits[ind]
+
+            # Get the letters corresponding to the current digit
+            curr_letter = digits[index]
             letters = hashmap[curr_letter]
-            
-            for i in letters:
-                path.append(i)
-                backtrack(ind + 1, path)
+
+            # Iterate through the letters and explore combinations
+            for letter in letters:
+                path.append(letter)
+                backtrack(index + 1, path)
                 path.pop()
-                
-                
-            
-        
-        
+
+        # Initialize an empty list to store the result
         res = []
-        
-        backtrack(0 , [])
+
+        # Start the backtracking process with initial values
+        backtrack(0, [])
+
+        # Return the final result
         return res
-        
-        
