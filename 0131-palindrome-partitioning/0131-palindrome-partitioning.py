74f0e8091e9ee3659375cb9b997d3f793dff3ffe
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-17 21:00:39 +0530

    Time: 484 ms (74.50%), Space: 36 MB (32.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,41 +1,26 @@
-from typing import List
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        # List to store the current partition
-        ds = []
-        # List to store all valid partitions
         res = []
-        
-        # Helper function to generate partitions
+        ds = []
         def generate(ind):
-            # If we reach the end of the string, add the current partition to the result
             if ind == len(s):
                 res.append(ds[:])
                 return
             
-            # Iterate through the string starting from the current index
-            for i in range(ind, len(s)):
-                # Check if the substring from current index to i is a palindrome
-                if isPal(s, ind, i):
-                    # Add the palindrome to the current partition
-                    ds.append(s[ind:i + 1])
-                    # Recursively generate partitions for the remaining substring
-                    generate(i + 1)
-                    # Remove the last added element to backtrack and explore other possibilities
+            for i in range(ind , len(s)):
+                if isPal(s,ind,i):
+                    ds.append(s[ind:i+1])
+                    generate(i+1)
                     ds.pop()
-                
-        # Helper function to check if a substring is a palindrome
-        def isPal(s, start, end):
+                    
+        def isPal(s,start, end):
             while start <= end:
                 if s[start] != s[end]:
                     return False
-                else:
-                    start += 1
-                    end -= 1
+                start += 1
+                end -= 1
             return True
         
-        # Start generating partitions from the beginning of the string
+        
         generate(0)
-        # Return the final list of valid partitions
         return res
