b852ed754781c2e6d7367b35050eb54638185713
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-19 21:11:17 +0530

    Time: 72 ms (41.76%), Space: 17.1 MB (51.79%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,52 +1,46 @@
 class Solution:
-    def isSafe1(self, row, col, board, n):
-        # check upper element
-        duprow = row
-        dupcol = col
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def solve(col, board, ans, n):
+            if col == n:
+                ans.append([''.join(row) for row in board])
+                return
+            
+            def isSafe(row, col, board, n):
+                duprow = row
+                dupcol = col
+                
+                while row >= 0 and col >= 0:
+                    if board[row][col] == 'Q':
+                        return False
+                    row -= 1
+                    col -= 1
+                    
+                row = duprow
+                col = dupcol
+                
+                while col >= 0:
+                    if board[row][col] == 'Q':
+                        return False
+                    col -= 1
+                    
+                row = duprow
+                col = dupcol
+                
+                while row < n and col >= 0:
+                    if board[row][col] == 'Q':
+                        return False
+                    row += 1
+                    col -= 1
 
+                return True
 
-        while row >= 0 and col >= 0:
-            if board[row][col] == 'Q':
-                return False
-            row -= 1
-            col -= 1
+            for row in range(n):
+                if isSafe(row, col, board, n):
+                    board[row][col] = 'Q'
+                    solve(col + 1, board, ans, n)
+                    board[row][col] = '.'
 
-
-        col = dupcol
-        row = duprow
-        while col >= 0:
-            if board[row][col] == 'Q':
-                return False
-            col -= 1
-
-
-        row = duprow
-        col = dupcol
-        while row < n and col >= 0:
-            if board[row][col] == 'Q':
-                return False
-            row += 1
-            col -= 1
-
-
-        return True
-
-
-    def solve(self, col, board, ans, n):
-        if col == n:
-            ans.append(list(board))
-            return
-
-
-        for row in range(n):
-            if self.isSafe1(row, col, board, n):
-                board[row] = board[row][:col] + 'Q' + board[row][col+1:]
-                self.solve(col+1, board, ans, n)
-                board[row] = board[row][:col] + '.' + board[row][col+1:]
-
-
-    def solveNQueens(self, n):
         ans = []
-        board = ['.'*n for _ in range(n)]
-        self.solve(0, board, ans, n)
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        solve(0, board, ans, n)
         return ans
