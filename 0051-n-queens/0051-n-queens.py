44a81f6d7be3953412370198e750a1a733ae2495
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-23 20:14:48 +0530

    Time: 48 ms (87.89%), Space: 17 MB (62.20%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,28 +1,26 @@
-from typing import List
-
 class Solution:
-    def solve(self, col, board, ans, leftrow, upperDiagonal, lowerDiagonal, n):
-        if col == n:
-            ans.append(["".join(row) for row in board])
-            return
-
-        for row in range(n):
-            if leftrow[row] == 0 and lowerDiagonal[row + col] == 0 and upperDiagonal[n - 1 + col - row] == 0:
-                board[row][col] = 'Q'
-                leftrow[row] = 1
-                lowerDiagonal[row + col] = 1
-                upperDiagonal[n - 1 + col - row] = 1
-                self.solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
-                board[row][col] = '.'
-                leftrow[row] = 0
-                lowerDiagonal[row + col] = 0
-                upperDiagonal[n - 1 + col - row] = 0
-
     def solveNQueens(self, n: int) -> List[List[str]]:
+        def solve(col, board, ans, leftrow, upperDia, lowerDia, n):
+            if col == n:
+                ans.append([''.join(row) for row in board])
+                return
+
+            for row in range(n):
+                if leftrow[row] == 0 and upperDia[row + col] == 0 and lowerDia[n - 1 + col - row] == 0:
+                    board[row][col] = 'Q'
+                    leftrow[row] = 1
+                    lowerDia[n - 1 + col - row] = 1  # Corrected this line
+                    upperDia[row + col] = 1  # Corrected this line
+                    solve(col + 1, board, ans, leftrow, upperDia, lowerDia, n)
+                    board[row][col] = '.'
+                    leftrow[row] = 0
+                    lowerDia[n - 1 + col - row] = 0
+                    upperDia[row + col] = 0
+
         ans = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
+        board = [['.' for i in range(n)] for i in range(n)]
         leftrow = [0] * n
-        upperDiagonal = [0] * (2 * n - 1)
-        lowerDiagonal = [0] * (2 * n - 1)
-        self.solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+        upperDia = [0] * (2 * n - 1)
+        lowerDia = [0] * (2 * n - 1)
+        solve(0, board, ans, leftrow, upperDia, lowerDia, n)
         return ans
