13c9223ca268a260dfc5a267b17060524a92e6da
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-24 22:02:28 +0530

    Time: 37 ms (99.57%), Space: 17 MB (56.19%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -7,20 +7,28 @@ class Solution:
 
             for row in range(n):
                 if leftrow[row] == 0 and upperDia[row + col] == 0 and lowerDia[n - 1 + col - row] == 0:
+                    # Place queen
                     board[row][col] = 'Q'
                     leftrow[row] = 1
-                    lowerDia[n - 1 + col - row] = 1  # Corrected this line
-                    upperDia[row + col] = 1  # Corrected this line
+                    # Update upper diagonal
+                    upperDia[row + col] = 1
+                    # Update lower diagonal
+                    lowerDia[n - 1 + col - row] = 1
+                    # Recur to the next column
                     solve(col + 1, board, ans, leftrow, upperDia, lowerDia, n)
+                    # Backtrack and remove the queen
                     board[row][col] = '.'
                     leftrow[row] = 0
+                    # Undo the updates to diagonals
                     lowerDia[n - 1 + col - row] = 0
                     upperDia[row + col] = 0
 
+        # Initialize variables
         ans = []
         board = [['.' for i in range(n)] for i in range(n)]
         leftrow = [0] * n
         upperDia = [0] * (2 * n - 1)
         lowerDia = [0] * (2 * n - 1)
+        # Start solving from the first column
         solve(0, board, ans, leftrow, upperDia, lowerDia, n)
         return ans
