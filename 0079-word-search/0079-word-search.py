2dd67a06f5dd3e3e64d4048281975d818787852a
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-26 21:59:03 +0530

    Time: 3132 ms (85.01%), Space: 16.6 MB (56.57%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,32 @@
-from typing import List
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(i, j, k):
-            if not (0 <= i < len(board) and 0 <= j < len(board[0])) or board[i][j] != word[k]:
-                return False
-            if k == len(word) - 1:
+        def search(i, j, n, m, board, word, k):
+            if k == len(word):
                 return True
-
-            tmp, board[i][j] = board[i][j], '/'
-            found = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or \
-                    dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
-            board[i][j] = tmp
-
-            return found
-
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if dfs(i, j, 0):
-                    return True
-
-        return False
+            if i < 0 or j < 0 or i == n  or j == m or board[i][j]  != word[k]:
+                return False    
+            
+            ch = board[i][j]
+            
+            board[i][j] = '#'
+            
+            op1 = search(i+1,j,n,m,board,word,k+1)
+            op2 = search(i,j+1,n,m,board,word,k+1)
+            op3 = search(i-1,j,n,m,board,word,k+1)
+            op4 = search(i,j-1,n,m,board,word,k+1)
+            
+            board[i][j] = ch
+            return op1 or op2 or op3 or op4 
+        
+        
+        
+        
+        n = len(board) # n
+        m = len(board[0])# m
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == word[0]:
+                    if search(i,j,n,m,board,word,0):
+                        return True
+                        
