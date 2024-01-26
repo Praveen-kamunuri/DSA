144f6d8ac4794a4f63c211b3e24453263d71e176
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-01-26 22:26:04 +0530

    Time: 3173 ms (84.64%), Space: 16.6 MB (62.60%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,13 +1,14 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def search(i, j, n, m, board, word, k):
+        def search(i,j,n,m,board,word,k):
             if k == len(word):
                 return True
-            if i < 0 or j < 0 or i == n  or j == m or board[i][j]  != word[k]:
-                return False    
+            
+            
+            if( i < 0 or j < 0  or  i == n or j == m or board[i][j] != word[k]):
+                return False
             
             ch = board[i][j]
-            
             board[i][j] = '#'
             
             op1 = search(i+1,j,n,m,board,word,k+1)
@@ -16,17 +17,15 @@ class Solution:
             op4 = search(i,j-1,n,m,board,word,k+1)
             
             board[i][j] = ch
-            return op1 or op2 or op3 or op4 
+            
+            return op1 or op2  or op3 or op4
+    
         
-        
-        
-        
-        n = len(board) # n
-        m = len(board[0])# m
+        n = len(board)
+        m = len(board[0])
         
         for i in range(n):
             for j in range(m):
-                if board[i][j] == word[0]:
-                    if search(i,j,n,m,board,word,0):
-                        return True
-                        
+                if search(i,j,n,m,board,word,0):
+                    return True
+        
