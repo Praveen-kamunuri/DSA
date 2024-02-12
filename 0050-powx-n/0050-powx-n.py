7daae07cbf6757b772134aecf31c00a21bd0e8ea
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-12 20:42:50 +0530

    Time: 38 ms (47.86%), Space: 16.6 MB (59.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,22 +1,23 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        # Handle negative exponent by converting it to positive
         nn = n
         ans = 1.0
         if nn < 0:
             nn = -1 * nn
         
+        # Binary exponentiation algorithm
         while nn > 0:  
-            if nn % 2 == 1:
+            if nn % 2 == 1:  # If the exponent is odd
                 ans = ans * x
-                nn = nn - 1
+                nn = nn - 1  # Reduce the exponent by 1
 
-            else:
+            else:  # If the exponent is even
                 x = x * x
-                nn = nn / 2
-                
+                nn = nn / 2  # Halve the exponent
+        
+        # If the original exponent was negative, take the reciprocal of the result
         if n < 0:
             ans = 1.0 / ans
+            
         return ans
-        
-            
-            
