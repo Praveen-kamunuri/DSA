9f147da0b138f881257fd9546065159ec9f192f2
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-12 20:42:19 +0530

    Time: 41 ms (28.21%), Space: 16.7 MB (59.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,29 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # Initialize the result to 1.0
-        ans = 1.0
-        
-        # Make a copy of n (nn) to handle negative exponents
         nn = n
-        
-        # If the exponent is negative, make it positive
+        ans = 1.0
         if nn < 0:
             nn = -1 * nn
         
-        # Iterate through the bits of the exponent
-        while nn:
-            # If the current bit is 1, multiply ans by x
-            if nn % 2:
+        while nn > 0:  
+            if nn % 2 == 1:
                 ans = ans * x
                 nn = nn - 1
-            # Square x and halve the exponent
+
             else:
                 x = x * x
-                nn = nn // 2
-        
-        # If the original exponent was negative, take the reciprocal of ans
+                nn = nn / 2
+                
         if n < 0:
             ans = 1.0 / ans
-        
-        # Return the final result
         return ans
+        
+            
+            
