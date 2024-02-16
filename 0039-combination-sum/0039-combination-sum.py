07fd7d9ff8d25c18314d31ccd1d1ce4cd907644f
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-16 20:25:20 +0530

    Time: 57 ms (45.95%), Space: 16.7 MB (57.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,32 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # Initialize an empty list to store the final combinations
-        ans = []
-        # Initialize an empty list to store the current combination being formed
-        ds = []
-
-        # Define a recursive function to find combinations
-        def findCombinations(ind, target):
-            # Base case: If the index reaches the end of the candidates list
-            if ind == len(candidates):
-                # If the target is reduced to 0, add the current combination to the result
-                if target == 0:
+        
+        def generate(n, ds, ans, target, ind, summ):
+            if ind == n:
+                if summ == target:
                     ans.append(ds[:])
                 return
-
-            # Check if the current candidate can be included in the combination
-            if candidates[ind] <= target:
-                # Include the current candidate in the combination
+            
+            if summ + candidates[ind] <= target:
                 ds.append(candidates[ind])
-                # Recursively call the function with updated index and target
-                findCombinations(ind, (target - candidates[ind]))
-                # Remove the last element to backtrack and explore other possibilities
+                generate(n, ds, ans, target, ind, summ + candidates[ind])
                 ds.pop()
-
-            # Move to the next candidate in the list
-            findCombinations(ind + 1, target)
-
-        # Start the recursive function with initial index and target
-        findCombinations(0, target)
-        # Return the final list of combinations
+                
+            
+            generate(n, ds, ans, target, ind + 1, summ)
+    
+        n = len(candidates)
+        ds = []
+        ans = []
+        
+        generate(n, ds, ans, target, 0, 0)
         return ans
