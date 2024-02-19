1cc2d95b4020a4d1cc3a808f2ddc90e4766dca44
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-19 21:38:34 +0530

    Time: 48 ms (16.44%), Space: 16.5 MB (65.88%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,39 +1,27 @@
-from typing import List
-
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        # List to store the final combinations
-        res = []
-        # List to temporarily store combinations during recursion
-        ds = []
         
-        # Recursive function to find combinations
-        def findComb(num, summ, size):
-            # If the desired size is reached
+        ds = []
+        ans = []
+        def generate(num, summ, size):
             if size == k:
-                # Check if the sum is equal to the target
                 if summ == n:
-                    res.append(ds[:])  # Append the current combination to the result
+                    ans.append(ds[:])
                 return
             
-            # Stop recursion if the number exceeds 9
             if num > 9:
                 return
             
-            # Include the current number in the combination
             ds.append(num)
             
-            # Recursive call with the next number, updated sum, and increased size
-            findComb(num + 1, summ + num, size + 1)
+            generate(num + 1, summ + num , size + 1)
             
-            # Backtrack: remove the last number to explore other possibilities
             ds.pop()
             
-            # Recursive call without including the current number
-            findComb(num + 1, summ, size)
+            generate(num + 1, summ, size)
             
-        # Start the recursion with the initial parameters
-        findComb(1, 0, 0)
+    
+        generate(1,0,0)
         
-        # Return the final list of combinations
-        return res
+        return ans
+        
