f84f3f3919ba5ff1ac24fa873162d2ace57d250f
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-22 20:47:34 +0530

    Time: 462 ms (74.00%), Space: 35.2 MB (59.95%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,30 +1,35 @@
-from typing import List
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         path = []
-
-
-        def partitionHelper(index: int):
-            if index == len(s):
+        
+        
+        
+        def generate(ind):
+            if ind == len(s):
                 res.append(path[:])
                 return
-            for i in range(index, len(s)):
-                if isPalindrome(s, index, i):
-                    path.append(s[index:i + 1])
-                    partitionHelper(i + 1)
+            
+            for i in range(ind , len(s)):
+                if ispal(s , ind, i):
+                    path.append(s[ind:i+1])
+                    
+                    generate(i + 1)
                     path.pop()
-
-
-        def isPalindrome(s: str, start: int, end: int) -> bool:
+                
+        
+        def ispal(s, start, end):
+            
             while start <= end:
                 if s[start] != s[end]:
                     return False
                 start += 1
                 end -= 1
+            
             return True
-
-
-        partitionHelper(0)
+                
+        generate(0)
         return res
+    
+        
+        
