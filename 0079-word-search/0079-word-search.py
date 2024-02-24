e9078c8d6f82a35129a9d1a419d9d813030d9236
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-24 22:08:41 +0530

    Time: 3240 ms (81.52%), Space: 16.6 MB (57.09%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,33 +1,42 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
-        
-        m = len(board)
-        n = len(board[0])
-        
-        def search(i, j, m, n, board, word, k):
+        # Define a recursive function for searching the word on the board
+        def search(i, j, n, m, board, word, k):
+            # If the entire word has been found, return True
             if k == len(word):
                 return True
-            
-            if i < 0 or j < 0 or i == m or j == n or board[i][j] != word[k]:
+
+            # Check if the current position is out of bounds or if the character at this position
+            # does not match the current character in the word
+            if i < 0 or j < 0 or i == n or j == m or board[i][j] != word[k]:
                 return False
-            
+
+            # Save the current character on the board, and mark it as visited using a temporary character '#'
             ch = board[i][j]
-            
-            board[i][j] = '*'
-            
-            top = search(i - 1, j, m, n, board, word, k+1)
-            bot = search(i + 1, j, m, n, board, word, k+1)
-            left = search(i, j - 1,m , n, board, word, k + 1)
-            right = search(i, j + 1, m, n, board, word, k+1)
-            
+            board[i][j] = '#'
+
+            # Recursively check in all four directions (up, down, left, right)
+            op1 = search(i + 1, j, n, m, board, word, k + 1)
+            op2 = search(i, j + 1, n, m, board, word, k + 1)
+            op3 = search(i - 1, j, n, m, board, word, k + 1)
+            op4 = search(i, j - 1, n, m, board, word, k + 1)
+
+            # Restore the original character on the board
             board[i][j] = ch
-            
-            return top or bot or left or right
-        
-        
-        for i in range(m):
-            for j in range(n):
-                if search(i, j, m, n, board, word, 0):
+
+            # Return True if any of the recursive calls returned True
+            return op1 or op2 or op3 or op4
+
+        # Get the dimensions of the board
+        n = len(board)
+        m = len(board[0])
+
+        # Iterate through each cell in the board and start the search from that cell
+        for i in range(n):
+            for j in range(m):
+                if search(i, j, n, m, board, word, 0):
                     return True
+
+        # If no match is found, return False
         return False
