a7833c9149da271c8c80ef1884ec7c354c24c527
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-02-27 21:17:46 +0530

    Time: 74 ms (38.80%), Space: 17 MB (88.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,9 +1,8 @@
-from typing import List
-
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-
-        def isSafe(row, col, board, n):
+        
+        
+        def isSafe(row, col, board, ans, n):
             duprow = row
             dupcol = col
             
@@ -13,7 +12,7 @@ class Solution:
                 
                 row -= 1
                 col -= 1
-            
+                
             row = duprow
             col = dupcol
             
@@ -21,30 +20,35 @@ class Solution:
                 if board[row][col] == 'Q':
                     return False
                 col -= 1
-            
-            row = duprow
+            row = duprow   
             col = dupcol
             
             while row < n and col >= 0:
                 if board[row][col] == 'Q':
                     return False
+                
                 row += 1
                 col -= 1
-                
             return True
+        
+        
+        
+        
         
         def solve(col, board, ans, n):
             if col == n:
                 ans.append([''.join(row) for row in board])
                 return
-            
             for row in range(n):
-                if isSafe(row, col, board, n):
+                if isSafe(row, col, board, ans, n):
                     board[row][col] = 'Q'
                     solve(col + 1, board, ans, n)
                     board[row][col] = '.'
+
         
+        
+        board = [['.'] * n  for i in range(n)]
         ans = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
         solve(0, board, ans, n)
         return ans
+        
