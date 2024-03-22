6883c046b0309a71b6a4837c369110008b5a9f30
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-03-22 10:30:06 +0530

    Time: 36 ms (63.37%), Space: 16.7 MB (78.24%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,24 +1,23 @@
-from typing import List
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
-        # Define a recursive function to generate subsets
-        def generate(n, ds, ans, ind):
-            # Base case: If the index reaches the length of nums,
-            # append the current subset to the answer and return
+        def solve(ind, ans, ds):
             if ind == n:
-                ans.append(ds[:])  # Append a copy of ds to ans (as it will be modified in subsequent steps)
+                ans.append(ds[:])
                 return
+                
+            ds.append(nums[ind])
+            solve(ind + 1, ans, ds)
+            ds.pop()
+            solve(ind + 1, ans, ds)
+            
+            
         
-            ds.append(nums[ind])  # Include the current element at index 'ind' in the subset
-            generate(n, ds, ans, ind + 1)  # Recursive call to generate subsets including the current element
-            ds.pop()  # Remove the current element to backtrack and generate subsets excluding the current element
-            generate(n, ds, ans, ind + 1)  # Recursive call to generate subsets excluding the current element
         
-        n = len(nums)  # Get the length of the input list nums
-        ds = []  # Initialize an empty list to store subsets
-        ans = []  # Initialize an empty list to store the final answer
+        n = len(nums)
+        ans = []
+        ds = []
         
-        generate(n, ds, ans, 0)  # Call the recursive function to generate subsets, starting from index 0
-        return ans  # Return the list of subsets
+        
+        solve(0, ans, ds)
+        return ans
