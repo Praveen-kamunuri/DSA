c3f2e1327a622dcf4a8cb1c3d77c1cd12f537ba2
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-03-30 19:56:32 +0530

    Time: 123 ms (75.87%), Space: 18.1 MB (31.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,23 +1,24 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        n = len(nums)
-        # Check if the list contains only one element, and it's already a zero
-        if n == 1 and nums[0] == 0:
-            return
+        
+        
         
         j = -1
-        # Find the index of the first zero in the list
-        for i in range(n):
+        for i in range(len(nums)):
             if nums[i] == 0:
                 j = i
                 break
-        # If no zero is found, return as there's nothing to move
+                
         if j == -1:
             return
                 
-        # Iterate through the list starting from the index of the first zero (j)
-        for i in range(j+1, n):
-            # If a non-zero element is encountered, swap it with the first zero
+        for i in range(j+1,len(nums)):
             if nums[i] != 0:
-                nums[j], nums[i] = nums[i], nums[j]
+                nums[j] , nums[i] = nums[i] , nums[j]
+                
                 j += 1
+            
+        
+                
+            
+        
