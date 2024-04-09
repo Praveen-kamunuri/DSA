71756b0c5babe58b490adf25e1477068ce18e061
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-04-09 23:28:18 +0530

    Time: 37 ms (42.19%), Space: 16.6 MB (37.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,16 +1,12 @@
-class Solution(object):
-    def rotateString(self, s, goal):
-        # Check if the lengths of s and goal are not equal
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        
         if len(s) != len(goal):
             return False
         
-        # Check if s and goal are the same, indicating a valid rotation
-        if s == goal:
-            return True
-        
-        # Check if goal is a substring of s concatenated with itself
-        if goal in s + s:
+        res = s + s
+        if goal in res:
             return True
         else:
-            # If none of the conditions are met, return False
             return False
+        
