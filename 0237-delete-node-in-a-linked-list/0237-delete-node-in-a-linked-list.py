00615c6685ee92ba3b04bbf8c3c071ba131178d9
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-04-27 22:38:14 +0530

    Time: 41 ms (38.57%), Space: 16.8 MB (13.41%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -6,14 +6,14 @@
 
 class Solution:
     def deleteNode(self, node):
-        # Check if the given node or its next node is None
-        if node is None or node.next is None:
-            # You cannot delete the last node or a node that doesn't exist.
-            return
-
-        # Copy the value of the next node to the current node
+        
+        if not node or node.next == None:
+            return 
+        
+        
         next_node = node.next
+        
         node.val = next_node.val
-
-        # Update the next pointer of the current node to point to the node after the next node
         node.next = next_node.next
+        
+        
