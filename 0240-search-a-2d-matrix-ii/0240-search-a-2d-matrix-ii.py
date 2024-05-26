31c29cd04ac086811d13b6bccf60fe3f990647fd
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-05-26 20:18:19 +0530

    Time: 141 ms (43.74%), Space: 22.8 MB (96.45%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,46 +1,21 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # Helper function to perform binary search on a sorted array
-        def binSearch(arr, target):
-            n = len(arr)
-            low = 0
-            high = n - 1
+        
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        row = 0
+
+        col = m - 1
+        
+        while row < n and col >= 0:
             
-            # Binary search loop
-            while low <= high:
-                mid = (low + high) // 2
-                
-                # Check if the mid element is the target
-                if arr[mid] == target:
-                    return True
-                
-                # If target is greater, ignore left half
-                elif arr[mid] < target:
-                    low = mid + 1
-                    
-                # If target is smaller, ignore right half
-                else:
-                    high = mid - 1
-                    
-            # Target is not present in the array
-            return False
-        
-        row = len(matrix)  # Number of rows in the matrix
-        
-        # Iterate through each row and apply binary search
-        for i in range(row):
-            if binSearch(matrix[i], target):
+            if matrix[row][col] == target:
                 return True
-        
-        # Target is not found in any row
+            
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+                
         return False
-
-# Time Complexity: O(M * log N)
-# Space Complexity: O(1)
-
-# Explanation:
-# - M is the number of rows in the matrix.
-# - N is the number of columns in each row.
-# - For each row, binary search takes O(log N) time.
-# - As there are M rows, the overall time complexity is O(M * log N).
-# - Space complexity is O(1) because we are not using any additional space that scales with the input size.
