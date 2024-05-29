41a2354b41a56c4de8fd2204fb375da155225323
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-05-30 00:14:04 +0530

    Time: 30 ms (89.24%), Space: 16.5 MB (65.80%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,24 +1,23 @@
-class Solution(object):
-    def reverse(self, x):
-        # Check if the number is negative
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        sign = 1
         if x < 0:
-            sign = -1  # Set the sign to -1
-            x = abs(x)  # Take the absolute value of x
+            sign = -1
+            
+            x = abs(x)
+        
+        c = 0
+        
+        while x > 0:
+            rem = x % 10
+            c = c * 10 + rem
+            x = x // 10
+            
+        res = c * sign
+        
+        if -2 ** 31 <= res <= (2 ** 31) - 1:
+            return res
         else:
-            sign = 1  # Set the sign to 1 (positive or zero)
-
-        num_str = str(x)  # Convert the number to a string
-        l = ""  # Initialize an empty string to store the reversed digits
-
-        # Iterate over the characters of num_str in reverse order
-        for i in range(len(num_str) - 1, -1, -1):
-            l = l + num_str[i]  # Concatenate the current character to l
-
-        digit = int(l)  # Convert the reversed string back to an integer
-
-        # Check if the reversed digit exceeds the maximum value of a signed 32-bit integer
-        if digit > 2 ** 31 - 1:
-            return 0  # Return 0 if it exceeds the range
-
-        # Return the reversed value of x multiplied by the sign
-        return sign * digit
+            return 0
+        
