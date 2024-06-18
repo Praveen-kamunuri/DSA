d5bf4fccbe512fb1eb500c3bcc70f2d3542096c7
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-06-18 21:05:46 +0530

    Time: 56 ms (5.77%), Space: 16.5 MB (81.95%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,16 +1,21 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
+        
         n = len(nums)
-        cnt = 0  # Counter for the number of times the array is rotated
         
-        # Iterate through the array to check if it's rotated
-        for i in range(1, n):
-            if nums[i-1] > nums[i]:
-                cnt += 1
+        peek = 0
         
-        # Check the transition between the last and first elements
+        for i in range(n-1):
+            if nums[i] > nums[i+1]:
+                peek += 1
+                
+        
         if nums[n-1] > nums[0]:
-            cnt += 1
+            peek += 1
+            
+        if peek <= 1:
+            return True
+        else:
+            return False
         
-        # If the counter is less than or equal to 1, it means the array is sorted and rotated
-        return cnt <= 1
+        
