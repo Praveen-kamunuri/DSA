29d15dfeff234b37b5ec0959b6c0f9a9d835bffa
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-06-26 22:33:56 +0530

    Time: 54 ms (31.65%), Space: 16.9 MB (80.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,32 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
+        # Early exit if lengths of s and t are different
         if len(s) != len(t):
             return False
         
         n = len(s)
         
+        # Initialize arrays to count character frequencies ('a' to 'z')
         s_arr = [0] * 26
-        
         t_arr = [0] * 26
         
+        # Count frequencies of characters in string s
         for i in range(n):
             s_arr[ord(s[i]) - ord('a')] += 1
-            
+        
+        # Count frequencies of characters in string t
+        for i in range(n):
             t_arr[ord(t[i]) - ord('a')] += 1
         
+        # Compare the two arrays
         return s_arr == t_arr
-            
-            
+
+    
+    """
+        Checks if two strings s and t are anagrams of each other.
+        
+        Time complexity: O(n)
+        Space complexity: O(1), since the arrays s_arr and t_arr are of fixed size (26)
+        where n is the length of the strings s and t.
+    """
