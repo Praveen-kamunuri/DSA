a75fd381f4d5398c964481a278cdad5cd9369a29
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-06-27 22:24:10 +0530

    Time: 54 ms (23.39%), Space: 16.6 MB (16.94%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,17 +1,17 @@
-class Solution(object):
-    def intToRoman(self, num):
-        # Define arrays for Roman numeral values and symbols
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
         val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         sym = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
         
-        i = 0  # Initialize a counter to loop through val and sym arrays
-        ans = ""  # Initialize an empty string to store the Roman numeral
-        
-        # Main loop to convert num to Roman numeral
+        i = 0
+        res = ''
         while num > 0:
+            
             while num >= val[i]:
-                ans += sym[i]  # Append the current symbol to the result string
-                num -= val[i]  # Subtract the corresponding value from num
-            i += 1  # Move to the next symbol and value in the arrays
-        
-        return ans  # Return the Roman numeral representation of num
+                
+                res += sym[i]
+                num -= val[i]
+                
+            i += 1
+        return res
