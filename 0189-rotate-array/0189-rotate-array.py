55cbb94fa8c6cc8d2de4bfd4197d85f28b55072b
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-06-28 22:27:48 +0530

    Time: 155 ms (71.08%), Space: 27.7 MB (94.56%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,12 +1,23 @@
+from typing import List
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         
-        n = len(nums)
         
-        k = k % n
+        n = len(nums)  # Get the length of the array
         
+        k = k % n  # Handle the case when k is greater than n
+        
+        # Step 1: Reverse the entire array
         nums[:] = nums[::-1]
         
+        # Step 2: Reverse the first k elements
         nums[:k] = nums[:k][::-1]
+        
+        # Step 3: Reverse the remaining n-k elements
         nums[k:] = nums[k:][::-1]
-        return nums
+        
+        
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(1) (in-place rotation)
