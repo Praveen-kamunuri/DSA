e14ef7562d43bded0699a8884100ef8aecc776e4
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-01 22:28:31 +0530

    Time: 51 ms (29.06%), Space: 16.6 MB (67.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -1,14 +1,15 @@
+from typing import List
+
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        
-        cnt = 0
+        cnt = 0  # Counter to track consecutive odd numbers
         
         for i in arr:
-            if i % 2 == 1:
-                cnt += 1
-                
-                if cnt == 3:
+            if i % 2 == 1:  # Check if the number is odd
+                cnt += 1  # Increment the count of consecutive odds
+                if cnt == 3:  # If we have found three consecutive odds
                     return True
             else:
-                cnt = 0
-        return False
+                cnt = 0  # Reset the count if the number is even
+        
+        return False  # Return False if no three consecutive odds are found
