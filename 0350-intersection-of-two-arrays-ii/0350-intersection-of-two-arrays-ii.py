021fd88e338370cbc14fdac139e57ff110a7ee98
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-02 22:18:51 +0530

    Time: 50 ms (48.69%), Space: 16.7 MB (71.02%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,22 +1,21 @@
+from typing import List
+
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = {}  # Dictionary to count occurrences of each element in nums1
+        res = []  # List to store the result (intersection elements)
         
-        hashmap = {}
-        
-        
-        res = []
-        
+        # Count the occurrences of each element in nums1
         for i in nums1:
             if i in hashmap:
                 hashmap[i] += 1
             else:
                 hashmap[i] = 1
                 
+        # Find the intersection elements with nums2
         for num in nums2:
             if num in hashmap and hashmap[num] > 0:
-                res.append(num)
-                
-                hashmap[num] -= 1
-        return res
-                
+                res.append(num)  # Append the element to the result list
+                hashmap[num] -= 1  # Decrease the count of the element in hashmap
         
+        return res
