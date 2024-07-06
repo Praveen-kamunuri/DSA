a84fc697385e5f93178c424965be7c4af8c8d18d
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-06 21:15:06 +0530

    Time: 35 ms (71.30%), Space: 16.5 MB (21.47%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        sett = set()
+        
+        def func(num):
+            res = 0
+            while num != 0:
+                rem = num % 10
+                num = num // 10 
+                res += rem ** 2
+            return res
+        
+        while n != 1:
+            n = func(n)
+            
+            if n in sett:
+                return False
+            
+            sett.add(n)
+        
+        return True
