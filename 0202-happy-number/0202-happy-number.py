8ec177a45fbb63af4551906dc09031d047b59e7d
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-06 21:17:22 +0530

    Time: 36 ms (63.88%), Space: 16.5 MB (21.47%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,8 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         
+        # Use a set to track numbers we've seen to detect cycles
         sett = set()
         
+        # Helper function to calculate the sum of the squares of the digits of num
         def func(num):
             res = 0
             while num != 0:
@@ -11,17 +13,19 @@ class Solution:
                 res += rem ** 2
             return res
         
-        
+        # Loop until n becomes 1 or we detect a cycle
         while n != 1:
-            
             n = func(n)
             
-            
-            
+            # If we've seen this number before, there's a cycle and n is not happy
             if n in sett:
                 return False
             
+            # Add the number to the set to track it
             sett.add(n)
-            
-        return True
         
+        # If we exit the loop because n is 1, it's a happy number
+        return True
+
+# Time Complexity (TC): O((log n)^2)
+# Space Complexity (SC): O(k) where k is the number of unique sums encountered before a cycle is detected or the number becomes 1
