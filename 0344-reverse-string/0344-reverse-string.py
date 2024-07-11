0662e34112e4fa46bc4d616605d4d9c3cd1f34d9
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-11 22:20:36 +0530

    Time: 175 ms (20.67%), Space: 20.8 MB (76.70%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,15 +4,24 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
+        # Get the length of the list
         n = len(s)
         
+        # Initialize two pointers: start at the beginning, end at the last element
         start = 0
         end = n - 1 
         
+        # Loop until the two pointers meet in the middle
         while start <= end:
-            s[start] , s[end] = s[end] , s[start]
+            # Swap the elements at the start and end pointers
+            s[start], s[end] = s[end], s[start]
             
+            # Move the start pointer one step to the right
             start += 1
+            # Move the end pointer one step to the left
             end -= 1
-        
-    
+
+
+
+# Time Complexity (TC): O(n)
+# The algorithm iterates through half of the list, so the time complexity is linear O(1) in relation to the
