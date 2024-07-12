90599586c3d58e04f48cc5b46e63d72224ec6acd
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-12 20:43:05 +0530

    Time: 306 ms (32.62%), Space: 36.8 MB (39.88%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,49 +3,47 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # Check if the linked list is empty or has only one element,
-        # which is considered a palindrome.
-        if not head or not head.next:
-            return True
-
-        # Define a function to reverse a linked list.
-        def revll(ptr):
+        
+        def revll(current):
             prev = None
             next_node = None
-            while ptr:
-                next_node = ptr.next
-                ptr.next = prev
-                prev = ptr
-                ptr = next_node
+            
+            while current:
+                next_node = current.next
+                current.next = prev
+                prev = current
+                current = next_node
             return prev
-
-        # Initialize slow and fast pointers for finding the middle of the list.
+        
+        
+        
+        
+        
+        if not head or not head.next:
+            return True
+        
         slow = head
         fast = head
-
-        # Move the pointers to find the middle.
+        
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-
-        # Reverse the second half of the list starting from slow.next.
-        slow.next = revll(slow.next)
-
-        # Move slow to the beginning of the reversed second half,
-        # and initialize a dummy pointer at the head of the original list.
-        slow =  slow.next
-        dummy = head
-
-        # Compare values from the first half with values from the reversed second half.
-        while slow:
-            if dummy.val != slow.val:
+            
+        new_head = revll(slow.next)
+        
+        
+        d1 = head
+        d2 = new_head
+        
+        while d2:
+            if d1.val != d2.val:
+                revll(new_head)
                 return False
-            else:
-                dummy = dummy.next
-                slow = slow.next
-
-        # If the loop completes without finding any mismatches, it's a palindrome.
+            
+            d1 = d1.next
+            d2 = d2.next
+            
         return True
+    
