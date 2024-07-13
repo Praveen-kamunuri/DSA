c230db1b32a901db5e0162b8bc7c1984914e1dd9
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-13 22:11:39 +0530

    Time: 39 ms (65.27%), Space: 18.7 MB (12.71%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -1,33 +1,36 @@
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
-#         self.val = val
+#         self.val = val = val
 #         self.next = next
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        
+        # Check if the list is empty or has only one node
         if not head or not head.next:
-            return head
+            return head  # No reordering needed
         
-        current = head
-        
+        # Initialize pointers for odd and even lists
         odd = head
         even = head.next
+        even_head = head.next  # To connect the end of odd list to the start of even list
         
-        even_head = head.next
-        
+        # Traverse the list, rearranging nodes into odd and even lists
         while even and even.next:
-            odd.next = odd.next.next
-            even.next = even.next.next
+            odd.next = odd.next.next  # Link current odd node to the next odd node
+            even.next = even.next.next  # Link current even node to the next even node
             
+            # Move odd and even pointers to the next nodes in their respective lists
             odd = odd.next
             even = even.next
-            
+        
+        # Connect the end of the odd list to the head of the even list
         odd.next = even_head
         
-        return current
-            
-        
-            
-        
+        return head  # Return the rearranged list's head
+
+# Time Complexity (TC): O(n)
+# The list is traversed once, so the time complexity is O(n), where n is the number of nodes in the list.
+
+# Space Complexity (SC): O(1)
+# The space complexity is O(1) because we are using a constant amount of extra space (pointers).
