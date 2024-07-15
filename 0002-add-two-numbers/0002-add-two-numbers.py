8d6f9df7f0cfee2f88dfc20b4ddb05ebd12fa785
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-15 20:12:54 +0530

    Time: 54 ms (62.84%), Space: 16.7 MB (22.27%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,25 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # Create a dummy node to serve as the head of the result list.
-        dummy = ListNode()
-        temp = dummy  # Initialize a temporary pointer to the dummy node.
-        carry = 0  # Initialize carry to 0.
-
-        # Loop until both input lists are empty and there is no carry left.
-        while l1 or l2 or carry:
-            # Calculate the sum of values from l1, l2, and carry.
-            summ = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
-            carry = summ // 10  # Calculate the carry for the next iteration.
-
-            # Create a new node with the value of (summ % 10) and add it to the result list.
-            temp.next = ListNode(summ % 10)
-            temp = temp.next  # Move the temp pointer to the newly added node.
-
-            # Move to the next nodes in l1 and l2 if they exist.
+        
+        dummy = ListNode(-1)
+        temp = dummy
+        carry = 0
+        
+        while l1 != None or l2 != None:
+            
+            summ = 0
             if l1:
+                summ += l1.val
                 l1 = l1.next
+                
             if l2:
+                summ += l2.val
                 l2 = l2.next
-
-        # Return the result list, starting from the node after the dummy node.
+            
+            summ += carry
+            
+            carry = summ // 10
+            
+            node = ListNode(summ % 10)
+            
+            temp.next = node
+            
+            temp = temp.next
+            
+        if carry:
+            node = ListNode(carry)
+            temp.next = node
+            
         return dummy.next
+            
