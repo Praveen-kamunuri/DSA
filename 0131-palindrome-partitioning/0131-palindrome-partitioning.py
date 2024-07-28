61825678412a6eec4c5068cc3dbfddd0e2a67be2
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-07-28 20:38:36 +0530

    Time: 496 ms (16.57%), Space: 35 MB (70.44%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,35 +1,31 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        path = []
+        res = []  # To store the final list of palindrome partitions
+        path = []  # To store the current path (current partition)
         
-        
-        
+        # Function to generate all partitions starting from index `ind`
         def generate(ind):
+            # If we have reached the end of the string, add the current partition to the result
             if ind == len(s):
-                res.append(path[:])
+                res.append(path[:])  # Append a copy of the current path
                 return
             
-            for i in range(ind , len(s)):
-                if ispal(s , ind, i):
-                    path.append(s[ind:i+1])
-                    
-                    generate(i + 1)
-                    path.pop()
-                
+            # Try to partition the string starting from `ind` to `i`
+            for i in range(ind, len(s)):
+                # If the substring s[ind:i+1] is a palindrome, proceed
+                if ispal(s, ind, i):
+                    path.append(s[ind:i+1])  # Add this palindrome substring to the current path
+                    generate(i + 1)  # Recur for the remaining substring
+                    path.pop()  # Backtrack and remove the last added palindrome substring
         
+        # Function to check if the substring s[start:end+1] is a palindrome
         def ispal(s, start, end):
-            
             while start <= end:
                 if s[start] != s[end]:
-                    return False
+                    return False  # Not a palindrome
                 start += 1
                 end -= 1
-            
-            return True
-                
-        generate(0)
-        return res
-    
+            return True  # It is a palindrome
         
-        
+        generate(0)  # Start the recursive generation from index 0
+        return res  # Return the final result
