008a59b6406dd154aff971ab73d4da7d572331f6
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-08-02 20:46:22 +0530

    Time: 40 ms (20.23%), Space: 16.7 MB (27.44%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,41 +1,46 @@
-from collections import deque
-
 class MyStack:
 
     def __init__(self):
-        # Initialize two deques (queues)
-        self.queue1 = deque()
-        self.queue2 = deque()
+        self.q1 = deque()
+        self.q2 = deque()
+        
 
     def push(self, x: int) -> None:
-        # Append element x to queue1
-        self.queue1.append(x)
+        
+        self.q1.append(x)
+        
 
     def pop(self) -> int:
-        # Move elements from queue1 to queue2, except the last one
-        while len(self.queue1) > 1:
-            self.queue2.append(self.queue1.popleft())
-        # The last element in queue1 is the top element of the stack
-        popped_element = self.queue1.popleft()
-        # Swap the names of queue1 and queue2
-        self.queue1, self.queue2 = self.queue2, self.queue1
-        return popped_element
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        pop_element = self.q1.popleft()
+        
+        self.q1 , self.q2 = self.q2 , self.q1
+        
+        
+        return pop_element
+        
 
     def top(self) -> int:
-        # Move elements from queue1 to queue2, except the last one
-        while len(self.queue1) > 1:
-            self.queue2.append(self.queue1.popleft())
-        # The last element in queue1 is the top element of the stack
-        top_element = self.queue1.popleft()
-        # Save the top element to return after pushing it back to queue2
-        self.queue2.append(top_element)
-        # Swap the names of queue1 and queue2
-        self.queue1, self.queue2 = self.queue2, self.queue1
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        top_element = self.q1.popleft()
+        
+        self.q2.append(top_element)
+        
+        self.q1 , self.q2 = self.q2 , self.q1
         return top_element
+        
 
     def empty(self) -> bool:
-        # Return True if queue1 is empty, else False
-        return not self.queue1
+        if len(self.q1) < 1:
+            return True
+        else:
+            return False
+        
+
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
