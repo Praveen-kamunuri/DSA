ee105d4b762220493f45abcc9bfe8f478940e4bf
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-08-04 20:25:23 +0530

    Time: 37 ms (42.40%), Space: 16.6 MB (51.65%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,42 +1,37 @@
 class MyQueue:
 
     def __init__(self):
-        self.stack_in = []
-        self.stack_out = []
-        
+        self.stack_in = []  # Stack for enqueue operations
+        self.stack_out = []  # Stack for dequeue operations
 
     def push(self, x: int) -> None:
+        # Push the element onto stack_in
         self.stack_in.append(x)
-        
-        
 
     def pop(self) -> int:
+        # Transfer elements from stack_in to stack_out if stack_out is empty
         self.transfer()
+        # Pop the element from stack_out
         pop_element = self.stack_out.pop()
         return pop_element
-        
 
     def peek(self) -> int:
+        # Transfer elements from stack_in to stack_out if stack_out is empty
         self.transfer()
+        # Return the top element of stack_out without popping it
         return self.stack_out[-1]
-        
 
     def empty(self) -> bool:
-        
-        return  not self.stack_in and not  self.stack_out
-        
-        
-        
-        
+        # Return True if both stack_in and stack_out are empty, otherwise False
+        return not self.stack_in and not self.stack_out
+
     def transfer(self):
+        # Move elements from stack_in to stack_out if stack_out is empty
         if not self.stack_out:
             while self.stack_in:
                 self.stack_out.append(self.stack_in.pop())
-                
-        
 
-
-# Your MyQueue object will be instantiated and called as such:
+# Example usage:
 # obj = MyQueue()
 # obj.push(x)
 # param_2 = obj.pop()
