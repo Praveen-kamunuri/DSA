d4fa2ad923f72660dd4c755037635260de6fe8e8
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-08-11 08:40:53 +0530

    Time: 41 ms (15.43%), Space: 16.5 MB (52.37%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,25 +1,27 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         
-        n = len(s)
+        n = len(s)  # Get the length of the string
         
-        word_len = 0
+        word_len = 0  # Variable to keep track of the length of the last word
         
-        i = n - 1
+        i = n - 1  # Start from the last character in the string
         
+        # Iterate backwards through the string
         while i >= 0:
             
-            
+            # If the current character is a space
             if s[i] == ' ':
+                # If we haven't started counting a word yet, just move to the next character
                 if word_len == 0:
                     i -= 1
+                # If we have started counting a word, return the length of that word
                 else:
                     return word_len
             else:
+                # If the current character is not a space, increment the word length
                 word_len += 1
                 i -= 1
-        return word_len
                 
-        
-        
-        
+        # If the loop ends, return the word length (for cases where the entire string is one word)
+        return word_len
