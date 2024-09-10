d78b995cf434dca2c0660eebd5dc3acb285d2788
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-09-10 22:45:56 +0530

    Time: 602 ms (20.43%), Space: 77.3 MB (94.99%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+        
+        
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            self._remove(node)
+            self._insert(node)
+            return node.val
+        
+        return -1
+        
+        
+        
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:        
+            self._remove(self.hashmap[key])
+        if len(self.hashmap) == self.capacity:
+            self._remove(self.tail.prev)
+        
+        self._insert(Node(key, value))
+            
+          
+        
+    def _insert(self, node):
+        self.hashmap[node.key] = node
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+        node.prev = self.head
+        
+
+    def _remove(self,node):
+        del self.hashmap[node.key]
+        node.next.prev = node.prev
+        node.prev.next = node.next
+        
+        
+        
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
