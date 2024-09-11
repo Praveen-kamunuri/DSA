1d48da803706a78f237a34a2b33e85b571317804
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-09-11 20:16:06 +0530

    Time: 605 ms (18.66%), Space: 78 MB (72.73%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,54 +1,55 @@
 class Node:
-    # Node class to represent each entry in the doubly linked list
-    def __init__(self, key: int, value: int):
+    def __init__(self, key, val):
         self.key = key
-        self.value = value
+        self.val = val
         self.prev = None
         self.next = None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        # Initialize the LRUCache with the given capacity
         self.capacity = capacity
-        self.map = {}  # HashMap to store the cache keys and corresponding nodes
-        self.head = Node(0, 0)  # Dummy head node for the doubly linked list
-        self.tail = Node(0, 0)  # Dummy tail node for the doubly linked list
-        self.head.next = self.tail  # Connect head to tail initially
+        self.hashmap = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
         self.tail.prev = self.head
+        
 
     def get(self, key: int) -> int:
-        # Retrieve the value from the cache, and update it as most recently used
-        if key in self.map:
-            node = self.map[key]
-            self._remove(node)  # Remove the node from its current position
-            self._insert(node)  # Insert it back as the most recently used
-            return node.value
-        return -1
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            self._remove(node)
+            self._insert(node)
+            return node.val
+        else:
+            return -1
+
 
     def put(self, key: int, value: int) -> None:
-        # Insert or update the value in the cache
-        if key in self.map:
-            # If the key is already in cache, remove the old node
-            self._remove(self.map[key])
-        if len(self.map) == self.capacity:
-            # If the cache is full, remove the least recently used (LRU) node
+        if key in self.hashmap:
+            self._remove(self.hashmap[key])
+        if len(self.hashmap) == self.capacity:
             self._remove(self.tail.prev)
-        # Insert the new node as the most recently used
         self._insert(Node(key, value))
-
-    def _remove(self, node: Node) -> None:
-        # Remove the node from the doubly linked list
-        del self.map[node.key]  # Remove it from the HashMap
-        node.prev.next = node.next  # Bypass the node from the list
+        
+        
+    def _insert(self, node):
+        self.hashmap[node.key] = node
+        node.next = self.head.next
+        self.head.next = node
+        node.prev = node.next.prev
+        node.next.prev = node
+        
+    
+    def _remove(self, node):
+        del self.hashmap[node.key]
+        node.prev.next = node.next
         node.next.prev = node.prev
+        
 
-    def _insert(self, node: Node) -> None:
-        # Insert the node as the most recently used, right after the head
-        self.map[node.key] = node  # Add it to the HashMap
-        node.next = self.head.next  # Point the node to the current first node
-        self.head.next.prev = node  # Connect the current first node back to this new node
-        self.head.next = node  # Move the head's next to this new node
-        node.prev = self.head  # Make the new node's prev point to the head
 
-# Time Complexity: O(1) for both get and put operations because the operations on the doubly linked list and HashMap are O(1).
-# Space Complexity: O(N), where N is the capacity of the cache, because we're storing N nodes in the doubly linked list and HashMap.
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
