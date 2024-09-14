b11d86e3d500ed6745f667adfad77e6bcf320be3
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-09-14 22:50:57 +0530

    Time: 579 ms (47.51%), Space: 78.2 MB (49.71%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,24 +2,23 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.next = None
         self.prev = None
-
-
+        self.next = None
+    
 class LRUCache:
+    
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.hashmap = {}
         self.head = Node(0, 0)
-        self.tail = Node(0, 0) 
+        self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
         
         
 
     def get(self, key: int) -> int:
-        
         if key in self.hashmap:
             node = self.hashmap[key]
             self._remove(node)
@@ -28,33 +27,27 @@ class LRUCache:
         else:
             return -1
         
-        
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
             self._remove(self.hashmap[key])
-        if len(self.hashmap) == self.capacity:
+        if self.capacity  == len(self.hashmap):
             self._remove(self.tail.prev)
-        
         self._insert(Node(key, value))
-        
         
     def _insert(self, node):
         self.hashmap[node.key] = node
         node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
         self.head.next = node
-        
+        node.prev = self.head
+        node.next.prev = node
         
     def _remove(self, node):
         del self.hashmap[node.key]
-        node.next.prev = node.prev
         node.prev.next = node.next
+        node.next.prev = node.prev
         
-
-
+        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
