2987a8ecf49466ea06e3320a174f5a52a8bbb9e0
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-09-23 11:42:34 +0530

    Time: 1337 ms (33.33%), Space: 53.9 MB (33.33%) - LeetHub

--- 3295-report-spam-message/3295-report-spam-message.py
+++ 3295-report-spam-message/3295-report-spam-message.py
@@ -1,14 +1,24 @@
 class Solution:
     def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
+        # Create a hashset to store banned words for quick lookup (O(1) average time complexity for set lookup)
         hashset = set()
+        
+        # Initialize a counter to track how many banned words are found in the message
         cnt = 0
+        
+        # Add each banned word to the hashset
         for i in bannedWords:
             hashset.add(i)
-        for word  in message:
+        
+        # Check each word in the message
+        for word in message:
+            # If the word is in the hashset, increment the counter
             if word in hashset:
                 cnt += 1
+            
+            # If we have found 2 or more banned words, return True (indicating spam)
             if cnt >= 2:
                 return True
-        return False
-                
         
+        # If fewer than 2 banned words are found, return False (not spam)
+        return False
