0fe100c912862e5961cb5de24e85f5ffeca7482e
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-10-01 21:44:49 +0530

    Time: 222 ms (57.79%), Space: 17.4 MB (74.17%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        
+        def solve(nums, k):
+            if k < 0:
+                return 0
+            n = len(nums)
+            l = 0
+            summ = 0
+            cnt = 0
+            
+            for r in range(n):
+                summ += nums[r]
+                
+                while summ > k:
+                    summ -= nums[l]
+                    l += 1
+                
+                # Accumulate the number of valid subarrays ending at `r`
+                cnt += (r - l + 1)
+            
+            return cnt
+        
+        return solve(nums, goal) - solve(nums, goal - 1)
