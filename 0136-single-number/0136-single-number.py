ddcfe0595dc11198af89834f0f3c0cf9c5cc62e5
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-11-07 21:54:25 +0530

    Time: 0 ms (100.00%), Space: 18.5 MB (97.77%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,10 @@
-from typing import List
-
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # Initialize the result variable to store the single number
+        n = len(nums)
         res = 0
-        
-        # Iterate through each number in the input list
-        for n in nums:
-            # XOR the current number with the result
-            # XORing a number with itself cancels out the number, leaving 0
-            # So, if the same number appears twice, it will cancel out and not affect the result
-            # If a number appears only once, XORing it with 0 will result in that number
-            res = res ^ n
-        
-        # After iterating through all numbers, 'res' will contain the single number
+        for i in range(n):
+            res ^= nums[i]
+            
         return res
+        
+        
