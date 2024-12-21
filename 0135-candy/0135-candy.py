ae004ad6da506f6946e7bce0c00d669156a6bafe
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2024-12-21 22:36:59 +0530

    Time: 19 ms (33.50%), Space: 19.9 MB (7.79%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,27 +5,34 @@ class Solution:
         
         left = [0] * n
         
-        right = [0] * n
+    
         
         left[0] = 1
         
-        right[n-1] = 1
-        
+           
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
                 left[i] = left[i - 1] + 1
             else:
                 left[i] = 1
+                
+        curr = 1
+        right = 1
+        summ = max(1, left[n - 1])
         
         for j in range(n - 2, -1, -1):
             if ratings[j] > ratings[j + 1]:
-                right[j] = right[j + 1] + 1
+                curr = right + 1
+                right = curr
+                
+                
             else:
-                right[j] = 1
+                curr = 1
+                right = 1
+            
+            summ = summ + max(left[j], curr)
+                
+                
+        return summ
         
-        total_chocolates = 0
-        for k in range(n):
-            total_chocolates += max(left[k], right[k])
-        return total_chocolates
-            
-            
+      
