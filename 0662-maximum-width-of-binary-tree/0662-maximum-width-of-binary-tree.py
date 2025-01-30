1221a883e0d313c810fc96e97cdba903afdbc11c
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-01-30 22:08:24 +0530

    Time: 7 ms (26.2%), Space: 18.4 MB (77.42%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -1,44 +1,44 @@
 from collections import deque
-from typing import Optional
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # If the root is null, the width is zero
+
         if not root:
             return 0
-        
-        # Initialize a variable to store the maximum width
+
+        q = deque([(root, 0)])
+
         max_width = 0
-        
-        # Use deque for efficient popping from the front
-        queue = deque([(root, 0)])
-        
-        # Perform level-order traversal
-        while queue:
-            level_size = len(queue)
-            _, first_pos = queue[0]  # Get the position of the first node in the level
-            _, last_pos = queue[-1]  # Get the position of the last node in the level
-            
-            # Update max_width by calculating the width of the current level
+
+        while q:
+            size = len(q)
+
+            first_node, first_pos = q[0]
+
+            last_node, last_pos = q[-1]
+
             max_width = max(max_width, last_pos - first_pos + 1)
-            
-            # Process all nodes at the current level
-            for _ in range(level_size):
-                node, pos = queue.popleft()
-                
-                # Add left child with updated position
+
+            for i in range(size):
+
+                node, pos = q.popleft()
+
                 if node.left:
-                    queue.append((node.left, 2 * pos))
-                
-                # Add right child with updated position
+                    q.append((node.left, 2 * pos + 1))
+
                 if node.right:
-                    queue.append((node.right, 2 * pos + 1))
-        
+                    q.append((node.right, 2 * pos + 2))
+
         return max_width
+
+
+        
+
+
+        
