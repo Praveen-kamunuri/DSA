01b5ebccbe4414b56b361f1dd1c2c9eed28dc43a
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-02-13 22:33:43 +0530

    Time: 131 ms (11.93%), Space: 21.2 MB (77.26%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -1,4 +1,5 @@
 from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -7,90 +8,82 @@ from collections import deque
 #         self.right = None
 
 class Codec:
-
     def serialize(self, root):
-        """Encodes a tree to a single string.
+        """Encodes a tree to a single string using level-order traversal (BFS).
         
         :type root: TreeNode
         :rtype: str
         """
 
+        # If the tree is empty, return an empty string
         if not root:
             return ""
 
-        s = ""
-
-        q = deque()
-        
+        s = ""  # String to store serialized data
+        q = deque()  # Queue for BFS traversal
         q.append(root)
 
         while q:
             node = q.popleft()
 
             if not node:
-                s += '#,'
-
+                s += '#,'  # Use '#' to denote null nodes
             else:
-                s += str(node.val) + ','
+                s += str(node.val) + ','  # Store node value
+                q.append(node.left)  # Add left child to queue
+                q.append(node.right)  # Add right child to queue
 
-                q.append(node.left)
-                q.append(node.right)
+        return s[:-1]  # Remove the trailing comma
 
-        return s[:-1]
-
-
-
-        
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
+        """Decodes the serialized string back to a binary tree.
         
         :type data: str
         :rtype: TreeNode
         """
 
+        # If the serialized data is empty, return None
         if not data:
             return None
 
-        q = deque()
+        q = deque()  # Queue to help reconstruct the tree
+        tokens = data.split(',')  # Split the serialized data
 
-        tokens = data.split(',')
-
-        root = TreeNode(int(tokens.pop(0)))
-
+        root = TreeNode(int(tokens.pop(0)))  # Create the root node
         q.append(root)
 
         while q and tokens:
-
             node = q.popleft()
 
             if tokens:
-
                 left_val = tokens.pop(0)
-
-                if left_val != '#':
+                if left_val != '#':  # If not null, create left child
                     left_node = TreeNode(int(left_val))
-
                     node.left = left_node
-
                     q.append(left_node)
 
             if tokens:
-
                 right_val = tokens.pop(0)
-
-                if right_val != '#':
+                if right_val != '#':  # If not null, create right child
                     right_node = TreeNode(int(right_val))
-
                     node.right = right_node
-
                     q.append(right_node)
 
         return root
 
-            
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+"""
+Time Complexity:
+- serialize: O(N), since we traverse each node once during BFS.
+- deserialize: O(N), since we reconstruct each node once.
+
+Space Complexity:
+- serialize: O(N), for storing the serialized output string.
+- deserialize: O(N), for storing the queue during BFS.
+"""
