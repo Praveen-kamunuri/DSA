6cb805d27ab97e6b5d97aafab43c9a2de70a9b47
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-05-18 12:37:23 +0530

    Time: 490 ms (80.54%), Space: 20.8 MB (37.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,30 @@
-class Solution(object):
-    def threeSum(self, nums):
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
         n = len(nums)
-        ans = []
-        nums.sort()  # Sort the input list
-        
-        for i in range(n):
-            if i != 0 and nums[i] == nums[i-1]:
-                continue  # Skip duplicates by continuing to the next iteration
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            else:
+                target = -nums[i]
+                left = i + 1
+                right = n - 1
+                while left < right:
+                    current_sum = nums[left] + nums[right]
+                    if current_sum == target:
+                        result.append([nums[i], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left+1]:
+                            left += 1  
+                        while left < right and  nums[right] == nums[right-1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif current_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+        return result
+
+
             
-            j = i + 1  # Start the second pointer at the element next to i
-            k = n - 1  # Start the third pointer at the last element
-            
-            while j < k:
-                trip_sum = nums[i] + nums[j] + nums[k]
-                
-                if trip_sum < 0:
-                    j += 1  # Move the second pointer to the right
-                elif trip_sum > 0:
-                    k -= 1  # Move the third pointer to the left
-                else:
-                    # Found a valid triple, add it to the answer
-                    temp = [nums[i], nums[j], nums[k]]
-                    ans.append(temp)
-                    j += 1
-                    k -= 1
-                    
-                    # Skip duplicates by incrementing the second pointer
-                    while j < k and nums[j] == nums[j-1]:
-                        j += 1
-                        
-                    # Skip duplicates by decrementing the third pointer
-                    while j < k and nums[k] == nums[k+1]:
-                        k -= 1
-        
-        return ans
