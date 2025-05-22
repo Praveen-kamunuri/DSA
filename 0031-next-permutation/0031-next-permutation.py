711fa8000146a2c03ba488e299de55c7e7a629df
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-05-22 22:49:40 +0530

    Time: 0 ms (100%), Space: 17.7 MB (85.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,28 +1,36 @@
-
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        def rev(arr):
+            n = len(arr)
+            left = 0
+            right = n - 1
+            while left < right:
+                arr[left], arr[right] = arr[right], arr[left]
+                left += 1
+                right -= 1
+            return arr
         n = len(nums)
         ind = -1
 
-        # Step 1: Find the first element from the right that is smaller than the next element.
-        for i in range(n - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
+        for i in range(n-2, -1, -1):
+            if nums[i] < nums[i+1]:
                 ind = i
                 break
-
-        # Step 2: If no such element exists, reverse the entire list.
+            
         if ind == -1:
-            nums.reverse()
-            return nums  # This line may not be needed as the list is reversed in-place.
+            nums[:] = rev(nums)
+            return
 
-        # Step 3: Find the smallest element to the right of `ind` that is greater than `nums[ind]`.
-        for i in range(n - 1, ind, -1):
+        for i in range(n-1, ind, -1):
             if nums[i] > nums[ind]:
-                # Swap the two elements.
                 nums[i], nums[ind] = nums[ind], nums[i]
                 break
+        
+        nums[ind+1:] = rev(nums[ind+1:])
 
-        # Step 4: Reverse the subarray to the right of `ind`.
-        nums[ind + 1:] = reversed(nums[ind + 1:])
 
-        return nums  # Return the modified list.
+        
