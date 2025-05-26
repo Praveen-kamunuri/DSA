53e29c3bb71f5fda7e0e4c5629c18cfc301ff3c9
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-05-26 22:27:00 +0530

    Time: 34 ms (5.35%), Space: 16.6 MB (100%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,14 +1,29 @@
+from typing import List
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def generate(s, left, right):
-            if left == 0 and right == 0:
-                result.append(s)
+        
+        # Define a recursive function to generate combinations of parentheses
+        def generate(n, ds, openn, close, string):
+            # Base case: If the count of open and close parentheses reaches n,
+            # append the generated string to the list of combinations
+            if n == openn == close:
+                ds.append(string)
                 return
-            if left > 0:
-                generate(s + '(', left - 1, right)
-            if right > left:
-                generate(s + ')', left, right - 1)
-
-        result = []
-        generate('', n, n)
-        return result
+            
+            # If the count of open parentheses is less than n, recursively call the function
+            # with an additional open parenthesis
+            if openn < n:
+                generate(n, ds, openn + 1, close, string + '(')
+            
+            # If the count of close parentheses is less than the count of open parentheses,
+            # recursively call the function with an additional close parenthesis
+            if close < openn:
+                generate(n, ds, openn, close + 1, string + ')')
+                
+        ds = []  # Initialize an empty list to store combinations
+        
+        # Call the recursive function to generate combinations
+        generate(n, ds, 0, 0,'')
+        
+        return ds  # Return the list of generated combinations
