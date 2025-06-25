89a7a3b50cb289e14c4a22f0e3d12e76eb830eba
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-06-25 22:00:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        def is_cycle(node, adj, visited, path_sum):
+            visited[node] = 1
+            path_sum[node] = 1
+
+            for neighbour in adj[node]:
+                if visited[neighbour] == 0:
+                    if is_cycle(neighbour, adj, visited, path_sum) == True:
+                        return True
+                elif path_sum[neighbour] == 1:
+                    return True
+            return False
+
+        adj = [[]for _ in range(numCourses)]
+
+        for a, b in prerequisites:
+            adj[b].append(a)
+
+        visited = [0] * numCourses
+        path_sum = [0] * numCourses
+
+        for i in range(numCourses):
+            if visited[i] == 0:
+                if is_cycle(i, adj, visited, path_sum) == True:
+                    return False
+            
+        return True
