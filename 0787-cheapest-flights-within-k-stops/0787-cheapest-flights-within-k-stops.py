34c294d62735856414bc797450d0102936d5f323
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-07-21 20:41:10 +0530

    Time: 7 ms (67.65%), Space: 19.3 MB (38.92%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -4,28 +4,41 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
 
         adj = [[]for _ in range(n)]
+
         for u, v, wt in flights:
             adj[u].append((v, wt))
-        distance = [sys.maxsize] * n
+
         q = deque()
-        distance[src] = 0
+
+        distance = [sys.maxsize] * n
+
         q.append((0, (src, 0)))
 
+        distance[src] = 0
+
         while q:
-            stops, node_with_cost = q.popleft()
-            node = node_with_cost[0]
-            cost = node_with_cost[1]
+            stops, node_with_delay = q.popleft()
+            node = node_with_delay[0]
+            delay = node_with_delay[1]
 
             if stops > k:
                 continue
-            else:
-                for neighbour, wt in adj[node]:
-                    if cost + wt < distance[neighbour] and stops <= k:
-                        distance[neighbour] = cost + wt
-                        q.append((stops + 1, (neighbour, cost + wt)))
+            
+            for neighbour, edge_wt in adj[node]:
+                 if delay + edge_wt < distance[neighbour] and stops <= k:
+                    distance[neighbour] =  delay + edge_wt
+                    q.append((stops + 1, (neighbour, delay + edge_wt)))
+
+        print(distance)
+
         if distance[dst] == sys.maxsize:
             return -1
         else:
             return distance[dst]
+        
+
+
+        
+
 
         
