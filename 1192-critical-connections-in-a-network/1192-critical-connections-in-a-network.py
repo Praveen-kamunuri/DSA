c7b7a81dcb7dfdd29fa3ef935ffe1beb4515bc3a
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-09-03 22:44:17 +0530

    Time: 294 ms (55.99%), Space: 66.4 MB (90.05%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -2,39 +2,43 @@ class Solution:
     def __init__(self):
         self.timer = 1
 
-    def dfs(self, node, parent, visited, adj, tin, low, bridges):
-        visited[node] = 1
-        tin[node] = self.timer
-        low[node] = self.timer
+    def dfs(self, node, parent, adj, tin, low, vis, bridges):
+        vis[node] = 1
+        tin[node] = low[node] = self.timer
         self.timer += 1
-        for neighbour in adj[node]:
-            if neighbour == parent:
+
+        for adj_node in adj[node]:
+            if adj_node == parent:
                 continue
-                
-            if visited[neighbour] == 0:
-                self.dfs(neighbour, node, visited, adj, tin, low, bridges)
-                low[node] = min(low[node], low[neighbour])
-                if low[neighbour] > tin[node]:
-                    bridges.append((neighbour, node))
+            
+            if vis[adj_node] == 0:
+                self.dfs(adj_node, node, adj, tin, low, vis, bridges)
+                low[node] = min(low[node], low[adj_node])
+                if low[adj_node] > tin[node]:
+                    bridges.append([node, adj_node]) 
+            
             else:
-                low[node] = min(low[node], low[neighbour])
+                low[node] = min(low[node], low[adj_node])
+
 
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-
 
         adj = [[]for _ in range(n)]
         for u, v in connections:
             adj[u].append(v)
             adj[v].append(u)
-        
+
         visited = [0] * n
+        
         tin = [0] * n
         low = [0] * n
+
         bridges = []
-        self.dfs(0, -1, visited, adj, tin, low, bridges)
+
+        self.dfs(0, -1, adj, tin, low, visited, bridges)
+
         return bridges
 
 
 
-        
         
