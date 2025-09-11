e8d13f44c9f628cc9b84f2ded54588eef924c9ce
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-09-11 22:54:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
 
-        def dfs(steps):
-            # If we reach exactly 0 steps left, it's one valid way
-            if steps == 0:
-                return 1
-            # If steps go negative, it's not valid
-            if steps < 0:
-                return 0
-            # If result is already computed, return it
-            if steps in memo:
-                return memo[steps]
-
-            # Take 1 step or 2 steps
-            memo[steps] = dfs(steps - 1) + dfs(steps - 2)
-            return memo[steps]
-
-        return dfs(n)
+        def backtrack(summ, ds, res, n):
+            if summ > n:
+                return 
+            
+            elif summ == n:
+                res.append(ds[:])
+                return
+            
+            else:
+                backtrack(summ + 2, ds, res, n)
+                backtrack(summ + 1, ds, res, n)
+            
+            
+        res = []
+        ds = []
+        backtrack(0, ds, res, n)
+        return len(res)
