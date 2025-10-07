62ab28b5d1f71a0d6b2874a676448f69be421230
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-10-07 20:48:32 +0530

    Time: 0 ms (100%), Space: 18.3 MB (73.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,21 +1,17 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        n = len(nums)   # Get the length of the input 'nums' list.
-        low = 0         # Initialize a low pointer to the beginning of the list.
-        high = n - 1    # Initialize a high pointer to the end of the list.
-        low_bound = n   # Initialize 'low_bound' to the length of the list (default if target is larger than all elements).
-        
-        while low <= high:
-            mid = (low + high) // 2   # Calculate the middle index.
 
+        n = len(nums)
+
+        low = 0
+        high = n - 1
+        while  low <= high:
+            mid = (low + high) // 2
             if nums[mid] == target:
-                return mid  # If the middle element is equal to the target, return the index of the target.
-
+                return mid
             elif nums[mid] < target:
-                low = mid + 1  # If the middle element is less than the target, update 'low' to search in the right half.
-
+                low = mid + 1
             else:
-                low_bound = mid   # If the middle element is greater than the target, update 'low_bound' and continue searching the left half.
-                high -= 1
-
-        return low_bound   # If the loop finishes, return the 'low_bound' which is the index where the target should be inserted.
+                high =  mid - 1
+        return low
+        
