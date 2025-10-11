bb3da6b44d898e4757d5be2656c7bdda26d4ffc1
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2025-10-11 20:11:17 +0530

    Time: 36 ms (0.08%), Space: 16.2 MB (100%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,44 +1,38 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        # Define ans array to store the result.
-        ans = []
-        n = len(matrix) # no. of rows
-        m = len(matrix[0]) # no. of columns
-        
-        # Initialize the pointers reqd for traversal.
-        top = 0
-        left = 0
-        bottom = n - 1
-        right = m - 1
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        n = len(matrix)  # Number of rows
+        m = len(matrix[0])  # Number of columns
 
-        # Loop until all elements are not traversed.
-        while (top <= bottom and left <= right):
-            # For moving left to right
+        top = 0  # Initialize the top row
+        left = 0  # Initialize the leftmost column
+        bottom = n - 1  # Initialize the bottom row
+        right = m - 1  # Initialize the rightmost column
+
+        ans = []  # Initialize the result list to store the spiral order
+
+        while top <= bottom and left <= right:
+            # Traverse the top row from left to right and append elements to ans
             for i in range(left, right + 1):
                 ans.append(matrix[top][i])
-
             top += 1
 
-            # For moving top to bottom.
+            # Traverse the rightmost column from top to bottom and append elements to ans
             for i in range(top, bottom + 1):
                 ans.append(matrix[i][right])
-
             right -= 1
 
-            # For moving right to left.
-            if (top <= bottom):
+            # Check if there's a bottom row to traverse
+            if top <= bottom:
+                # Traverse the bottom row from right to left and append elements to ans
                 for i in range(right, left - 1, -1):
                     ans.append(matrix[bottom][i])
-
                 bottom -= 1
 
-            # For moving bottom to top.
-            if (left <= right):
+            # Check if there's a leftmost column to traverse
+            if left <= right:
+                # Traverse the leftmost column from bottom to top and append elements to ans
                 for i in range(bottom, top - 1, -1):
                     ans.append(matrix[i][left])
-
                 left += 1
 
-        return ans
-                
-        
+        return ans  # Return the list containing elements in spiral order
