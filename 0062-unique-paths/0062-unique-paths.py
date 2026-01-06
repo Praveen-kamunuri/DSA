729e9c5ac9c0bc9fd6f28c54a1cefaee28762e0b
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-01-06 22:57:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (7.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,24 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        dp = [[-1 for _ in range(n)]for _ in range(m)]
-        def cal_paths(row, col, m, n):
-            if row == 0 and col == 0:
-                return 1
+        dp = [[0 for _ in range(n)]for _ in range(m)]
 
+        dp[0][0] = 1
+        
+        for i in range(0, m):
+            for j in range(0, n):
 
-            if row < 0 or col < 0:
-                return 0
+                if i == 0 and j == 0:
+                    continue
+                else:
+                    go_up = dp[i - 1][j]
+                    go_right = dp[i][j - 1]
 
-            if dp[row][col] != -1:
-                return dp[row][col]
+                    dp[i][j] = go_up + go_right
 
-            go_up = cal_paths(row - 1, col, m, n)
-            go_left = cal_paths(row, col - 1, m, n)
-
-            dp[row][col] = go_up + go_left
-
-            return dp[row][col]
-    
-        return cal_paths(m - 1, n -1, m, n)
+        return dp[m - 1][n - 1]
         
