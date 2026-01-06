54caa4d5a882b4f6224570f3b3bb256b8ffe13b1
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-01-06 23:23:30 +0530

    Time: 3 ms (23.6%), Space: 19.4 MB (7.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,32 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        # dp[i][j] represents number of unique paths
-        # to reach cell (i, j) from (0, 0)
-        # Space Complexity: O(m * n) → 2D DP table
-        dp = [[0 for _ in range(n)]for _ in range(m)]
+        prev = [0] * n
 
-        # Base case: only 1 way to start at (0, 0)
-        dp[0][0] = 1
-        
-        # Time Complexity:
-        # Two nested loops running over m rows and n columns
-        # TC = O(m * n)
-        for i in range(0, m):
-            for j in range(0, n):
+        prev[0] = 1
 
-                # Skip starting cell as it is already initialized
+        for i in range(m):
+            temp = [0] * n
+            for j in range(n):
+                
                 if i == 0 and j == 0:
-                    continue
+                    temp[j] = 1
                 else:
-                    # Number of ways from top cell
-                    go_up = dp[i - 1][j]
-
-                    # Number of ways from left cell
-                    go_right = dp[i][j - 1]
-
-                    # Total ways to reach current cell
-                    dp[i][j] = go_up + go_right
-
-        # Answer is stored in bottom-right cell
-        return dp[m - 1][n - 1]
+                    temp[j] = prev[j] + temp[j - 1]
+            prev = temp
+        return temp[n - 1]
