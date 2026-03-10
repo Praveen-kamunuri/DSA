4a12d8e9b400d498d1eaee7b94e06bfd5b080373
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-03-10 21:17:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,29 +1,21 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        def count_comb(ind, target, arr, dp):
-            if ind == 0:
-                if target % arr[0] == 0:
-                    return 1
-                return 0
-
-            if dp[ind][target] != -1:
-                return dp[ind][target]
-
-            take = 0
-            if arr[ind] <= target:
-                take = count_comb(ind, target - arr[ind], arr, dp)             
-            
-            not_take = count_comb(ind - 1, target, arr, dp)
-
-            dp[ind][target] = take + not_take
-
-            return dp[ind][target]
-            
         n = len(coins)
 
-        dp = [[-1] * (amount + 1)for _ in range(n)]
+        dp = [[0] * (amount + 1)for _ in range(n)]
 
+        for t in range(amount + 1):
+            if t % coins[0] == 0:
+                dp[0][t] = 1
 
-        res = count_comb(n - 1, amount, coins, dp)
-        return res
+        for ind in range(1, n):
+            for target in range(amount + 1):
+                take = 0
+                if coins[ind] <= target:
+                    take = dp[ind][target - coins[ind]]
+                not_take = dp[ind - 1][target]
+
+                dp[ind][target] = take + not_take
+        
+        return dp[n - 1][amount]
