70ced2cfabd9f0ecedd6b58663dc4f87f23f4df9
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-03-26 21:54:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,26 +1,55 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-
+        
+        # INTUITION:
+        # Instead of generating all subsequences and checking palindrome (costly),
+        # we use a trick:
+        #
+        # Longest Palindromic Subsequence (LPS) = LCS(s, reverse(s))
+        #
+        # Why?
+        # Because a palindrome reads same forward & backward,
+        # so matching s with its reverse gives longest palindromic subsequence.
+        #
+        # Once we get LPS length:
+        # Minimum insertions = total length - LPS
+        #
+        # Because we only need to insert missing characters to make it palindrome
+        
+        
         n = len(s)
-
-        rev_s = s[::-1]
-
-       
-        def findLCS(s, rev_s, n):
-
-            dp = [[0] * (n + 1)for _ in range(n + 1)]
-
+        rev = s[::-1]   # reverse of string
+        
+        
+        # Function to find LCS of s and rev
+        def lcs(str1, str2, n):
+            
+            # dp[i][j] = LCS length of str1[0:i] and str2[0:j]
+            dp = [[0] * (n + 1) for _ in range(n + 1)]
+            
             for i in range(1, n + 1):
                 for j in range(1, n + 1):
-                    if s[i - 1] == rev_s[j - 1]:
+                    
+                    # If characters match -> take diagonal +1
+                    if str1[i - 1] == str2[j - 1]:
                         dp[i][j] = 1 + dp[i - 1][j - 1]
-
+                    
+                    # Else take max of left or top
                     else:
                         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
+            
             return dp[n][n]
+        
+        
+        # Longest Palindromic Subsequence length
+        lps = lcs(s, rev, n)
+        
+        # Minimum insertions needed
+        return n - lps
 
 
-        longest_pal =  findLCS(s, rev_s, n)
+# Time Complexity: O(n^2)
+# - We fill an n x n DP table
 
-        return n - longest_pal
+# Space Complexity: O(n^2)
+# - DP table of size n x n
