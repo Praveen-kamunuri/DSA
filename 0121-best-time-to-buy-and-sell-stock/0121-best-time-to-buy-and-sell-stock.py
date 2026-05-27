e7bccca867862ddfd3db1834c7a342c0f7ad5279
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-05-27 23:27:18 +0530

    Time: 61 ms (29.53%), Space: 28.7 MB (21.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,23 +1,20 @@
-from typing import List
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)  # Length of the prices list
 
-        mini = prices[0]  # Initialize the minimum price to the first price
+        n = len(prices)
 
-        profit = 0  # Initialize the maximum profit to 0
+        mini = prices[0]
+
+        profit = 0
 
         for i in range(1, n):
-            cost = prices[i] - mini  # Calculate potential profit if selling at prices[i]
+            cost = prices[i] - mini
+            profit = max(profit, cost)
+            mini = min(mini, prices[i])
+        return profit
 
-            profit = max(profit, cost)  # Update maximum profit if the current cost is higher
 
-            mini = min(prices[i], mini)  # Update minimum price to the lower of current price or previous minimum
 
-        return profit  # Return the maximum profit found
 
-        # Time Complexity: O(n)
-        # We iterate through the list once, performing constant time operations within the loop.
-        # Space Complexity: O(1)
-        # We use a fixed amount of extra space (variables `mini` and `profit`), regardless of the input size.
+
+        
