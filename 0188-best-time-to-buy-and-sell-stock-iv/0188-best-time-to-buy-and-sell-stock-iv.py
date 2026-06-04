8dd57902887c026e77fb199611026d9becb2695a
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-06-04 22:49:04 +0530

    Time: 183 ms (19.88%), Space: 34.3 MB (17.02%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+
+        n = len(prices)
+
+        # dp = [[[-1]for _ in range(k + 1)]for _ in range(2)for _ in range(n)]
+
+        dp = [[[-1 for _ in range(k)]for _ in range(2)]for _ in range(n)]
+
+        def calProfit(ind, canBuy, totalTrades, prices, n, k):
+
+            if ind == n or totalTrades == k:
+                return 0
+
+            if dp[ind][canBuy][totalTrades] != -1:
+                return dp[ind][canBuy][totalTrades]
+
+            if canBuy == 1:
+                profit = max(
+                    -prices[ind] + calProfit(ind + 1, 0, totalTrades, prices, n, k),
+                    0 + calProfit(ind + 1, 1, totalTrades, prices, n, k)
+                )
+
+                dp[ind][canBuy][totalTrades] = profit
+                return profit
+
+            else:
+                profit = max(
+                    prices[ind] + calProfit(ind + 1, 1, totalTrades + 1, prices, n, k),
+                    0 + calProfit(ind + 1, 0, totalTrades, prices, n, k)
+                )
+
+                dp[ind][canBuy][totalTrades] = profit
+                return profit
+        
+        return calProfit(0, 1, 0, prices, n, k)
+
+
+        
