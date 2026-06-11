edc4a425e020aea4c451db441fd53d7f1da3417e
KAMUNURI PRAVEEN <129310014+Praveen-kamunuri@users.noreply.github.com>
2026-06-11 21:53:36 +0530

    Time: 4792 ms (5%), Space: 148.3 MB (5.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,7 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
 
-
         n = len(nums)
 
         dp = [[0 for _ in range(n + 1)]for _ in range(n + 1)]
@@ -12,17 +11,10 @@ class Solution:
                 not_pick = 0 + dp[ind + 1][prev_ind + 1]
 
                 pick = 0
- 
+
                 if prev_ind == -1 or nums[prev_ind] < nums[ind]:
                     pick = 1 + dp[ind + 1][ind + 1]
-
+                
                 dp[ind][prev_ind + 1] = max(pick, not_pick)
 
         return dp[0][0]
-
-
-        
-
-
-
-        
